976d54966df6b1538cf12557fec215e5271d26fc
Ashwin Ramaswamy <aramaswamy@palantir.com>
2013-08-22 09:55:40 -0400

    label tests

---
+++ test/rules/labelPosRuleTests.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/// <reference path='../references.ts' />
+
+describe("<labelpos>", () => {
+    it("enforces that labels are correctly positioned", () => {
+        var fileName = "rules/labelpos.test.ts";
+        var createFailure = Lint.Test.createFailuresOnFile(fileName, Lint.Rules.LabelPosRule.FAILURE_STRING);
+        var expectedFailures: Lint.RuleFailure[] = [
+            createFailure([2, 5], [2, 9]),
+            createFailure([5, 5], [5, 9])
+        ];
+        var actualFailures = Lint.Test.applyRuleOnFile(fileName, "labelpos");
+
+        Lint.Test.assertFailuresEqual(actualFailures, expectedFailures);
+    });
+});
