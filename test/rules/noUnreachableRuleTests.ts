ad02e7799032037c5c76a4f80cf0c8dfffd6e5ac
Ashwin Ramaswamy <aramaswamy@palantir.com>
2013-08-22 09:58:45 -0400

    unreachable code tests

---
+++ test/rules/noUnreachableRuleTests.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2013 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/// <reference path='../references.ts' />
+
+describe("<nounreachable>", () => {
+    it("restricts the use of unreachable code statements", () => {
+        var fileName = "rules/nounreachable.test.ts";
+        var createFailure = Lint.Test.createFailuresOnFile(fileName, Lint.Rules.NoUnreachableRule.FAILURE_STRING);
+        var expectedFailures: Lint.RuleFailure[] = [
+            createFailure([6, 5], [6, 11]),
+            createFailure([13, 9], [13, 15]),
+            createFailure([25, 9], [25, 27]),
+            createFailure([28, 9], [28, 15])
+        ];
+        var actualFailures = Lint.Test.applyRuleOnFile(fileName, "nounreachable");
+
+        Lint.Test.assertFailuresEqual(actualFailures, expectedFailures);
+    });
+});
