239085dbe469bff2cde8ccea68f83d59793edc45
Adam Kalman <akalman@daptiv.com>
2013-10-24 15:18:08 -0700

    add typedef rule.

---
+++ test/files/rules/typedef.test.ts
@@ -0,0 +1,34 @@
+var NoTypeObjectLiteralWithPropertyGetter = {
+    Prop: "some property",
+
+    get Prop() {
+        return Prop;
+    }
+};
+
+interface NoTypeInterface {
+    Prop;
+}
+
+var NoTypesFn = function (
+    a,
+    b) {
+    var c = a + b;
+    var d = a - b;
+
+    try {
+        return c / d;
+    } catch (ex) {
+        console.log(ex);
+    }
+};
+
+class NoTypesClass {
+    [index]
+
+    Prop = "some property";
+
+    public get name() {
+        return "some name";
+    }
+}
