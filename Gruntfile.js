c19ba34f4c1e6bc54764551840502bb8e76860bf
Ashwin Ramaswamy <aramaswamy@palantir.com>
2014-10-27 14:21:08 -0400

    remove typescript from build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,10 @@ module.exports = function(grunt) {
   grunt.initConfig({    
     pkg: grunt.file.readJSON('package.json'),
 
-    // typescript source folders from the compiler's Jakefile
-    ts_compilerDirectory: "src/typescript/src/compiler",
-    ts_servicesDirectory: "src/typescript/src/services",
-
     clean: {
       bin: ['bin/tslint-cli.js'],
       core: ['build/rules/', 'build/formatters', 'lib/tslint.*'],
       test: ['build/test/'],
-      typescript: ['lib/typescript.*']
     },
 
     concat: {
@@ -98,29 +93,6 @@ module.exports = function(grunt) {
       test: {
         src: ['test/**/*.ts', '!test/files/**/*.ts'],
         out: 'build/tslint-tests.js'
-      },
-
-      typescript: {
-        options: {
-          declaration: true
-        },
-        src: [
-          "<%= ts_compilerDirectory %>/core.ts",
-          "<%= ts_compilerDirectory %>/sys.ts",
-          "<%= ts_compilerDirectory %>/types.ts",
-          "<%= ts_compilerDirectory %>/scanner.ts",
-          "<%= ts_compilerDirectory %>/parser.ts",
-          "<%= ts_compilerDirectory %>/binder.ts",
-          "<%= ts_compilerDirectory %>/checker.ts",
-          "<%= ts_compilerDirectory %>/emitter.ts",
-          "<%= ts_compilerDirectory %>/diagnosticInformationMap.generated.ts",
-
-          "<%= ts_servicesDirectory %>/breakpoints.ts",
-          "<%= ts_servicesDirectory %>/services.ts",
-          "<%= ts_servicesDirectory %>/shims.ts",
-          "<%= ts_servicesDirectory %>/outliningElementsCollector.ts"
-        ],
-        out: 'lib/typescript.js'
       }
     }
   });
@@ -133,11 +105,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-ts');
 
   // register custom tasks
-  grunt.registerTask('typescript', ['clean:typescript', 'ts:typescript']);
   grunt.registerTask('core', ['clean:core', 'ts:core', 'ts:core_rules', 'ts:core_formatters']);
   grunt.registerTask('bin', ['clean:bin', 'ts:bin', 'tslint:src']);
   grunt.registerTask('test', ['clean:test', 'ts:test', 'tslint:test', 'concat:test', 'mochaTest']);
 
   // create default task
-  grunt.registerTask('default', ['typescript', 'core', 'bin', 'test']);
+  grunt.registerTask('default', ['core', 'bin', 'test']);
 };
