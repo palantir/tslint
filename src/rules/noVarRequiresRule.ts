6d85350203b96dd8100593b480db05dcffa5aaca
Adi Dahiya <adahiya@palantir.com>
2015-01-21 20:28:43 -0500

    make RequiresWalker implemenation more readable

--- src/rules/noVarRequiresRule.ts
+++ src/rules/noVarRequiresRule.ts
@@ -38,11 +38,12 @@ class RequiresWalker extends Lint.ScopeAwareRuleWalker<{}> {
     public visitCallExpression(node: ts.CallExpression) {
         var expression = node.expression;
 
-        if (this.getCurrentDepth() <= 1 &&
-                expression.kind === ts.SyntaxKind.Identifier &&
-                node.getFirstToken().getText() === "require") {
-            // if we're calling (invoking) require, then it's not part of an import statement
-            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+        if (this.getCurrentDepth() <= 1 && expression.kind === ts.SyntaxKind.Identifier) {
+            var identifierName = (<ts.Identifier> expression).text;
+            if (identifierName === "require") {
+                // if we're calling (invoking) require, then it's not part of an import statement
+                this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+            }
         }
 
         super.visitCallExpression(node);
