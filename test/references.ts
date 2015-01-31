7d49a87a8acefbc4799145243704fd4ffa5abdae
Ashwin Ramaswamy <aramaswamy@palantir.com>
2015-01-31 16:03:05 -0500

    remove unnecessary references

--- test/references.ts
+++ test/references.ts
@@ -14,11 +14,7 @@
  * limitations under the License.
 */
 
-/// <reference path='../src/tslint.ts'/>
 /// <reference path='helper.ts'/>
-/// <reference path='typings/chai.d.ts'/>
-/// <reference path='typings/chai-assert.d.ts'/>
-/// <reference path='typings/mocha.d.ts'/>
 
 // attach chai.assert to the global object
 global.assert = require("chai").assert;
