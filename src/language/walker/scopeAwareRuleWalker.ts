9ef205fe5e6d77712f465eb9e39bc682905fef73
Wesley Wigham <wwigham@gmail.com>
2015-10-12 03:55:59 -0700

    Fix linter errors

--- src/language/walker/scopeAwareRuleWalker.ts
+++ src/language/walker/scopeAwareRuleWalker.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import * as Lint from "../../lint";
 import {RuleWalker} from "./ruleWalker";
 import * as ts from "typescript";
 
