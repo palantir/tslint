311bc4a359db3c20cd86886a6192650ec9ad27ba
Andrew Z Allen <me@andrewzallen.com>
2015-11-24 20:11:31 -0700

    Standardize license docs as JSDoc.
    JSDoc has a @license annotation. This change modifies all .ts files and
    adds @license to every annotation. It also switches to a real JSDoc
    leading comment /** instead of just /*. I also added license annotations
    to a few files (with copyright year 2015).

--- src/rules/labelUndefinedRule.ts
+++ src/rules/labelUndefinedRule.ts
@@ -1,4 +1,5 @@
-/*
+/**
+ * @license
  * Copyright 2013 Palantir Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 import * as ts from "typescript";
 import * as Lint from "../lint";
