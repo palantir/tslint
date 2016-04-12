af6bfdd823079b85c09c8da22c47ca73c46fe561
Jason Killian <JKillian@users.noreply.github.com>
2016-04-12 14:11:42 -0400

    Prepare release v3.7.0-dev.5 (#1119)

--- src/tslint.ts
+++ src/tslint.ts
@@ -33,7 +33,7 @@ import {loadRules} from "./ruleLoader";
 import {arrayify} from "./utils";
 
 class Linter {
-    public static VERSION = "3.7.0-dev.4";
+    public static VERSION = "3.7.0-dev.5";
     public static findConfiguration = findConfiguration;
     public static findConfigurationPath = findConfigurationPath;
     public static getRulesDirectories = getRulesDirectories;
