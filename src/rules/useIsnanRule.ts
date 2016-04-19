71d4b953e51b1dabe64f3406f31ef11fdd47438b
Hamlet D'Arcy <hamletdrc@gmail.com>
2016-04-19 22:06:20 +0200

    Issue #1054 - Ported the use-isnan rule from tslint-microsoft-contrib to tslint (#1140)
    Closes #1054

---
+++ src/rules/useIsnanRule.ts
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright 2013 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as ts from "typescript";
+import * as Lint from "../lint";
+
+/**
+ * Implementation of the use-isnan rule.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "Found an invalid comparison for NaN: ";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new UseIsnanRuleWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class UseIsnanRuleWalker extends Lint.RuleWalker {
+
+    protected visitBinaryExpression(node: ts.BinaryExpression): void {
+        if (this.isExpressionNaN(node.left) || this.isExpressionNaN(node.right)) {
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING + node.getText()));
+        }
+        super.visitBinaryExpression(node);
+    }
+
+    private isExpressionNaN(node: ts.Node) {
+        return node.kind === ts.SyntaxKind.Identifier && node.getText() === "NaN";
+    }
+}
