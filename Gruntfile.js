f74bd30e0f0c921d6ec077db1dded005a22af176
Jay Anslow <jay@anslow.me.uk>
2016-04-27 11:15:27 +0100

    Fix code style issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
                 cmd: "npm",
                 args: ["install"],
                 options: {
-                  cwd: "./test/config"
+                    cwd: "./test/config"
                 }
             },
             testBin: {
