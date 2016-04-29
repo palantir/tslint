7a574c243e61ff75335fe36b582b093a9e025ac4
Hamlet D'Arcy <hamda@microsoft.com>
2016-04-29 07:43:20 +0200

    use-isnan fixed to ignore assignments to NaN

--- src/rules/useIsnanRule.ts
+++ src/rules/useIsnanRule.ts
@@ -32,7 +32,8 @@ export class Rule extends Lint.Rules.AbstractRule {
 class UseIsnanRuleWalker extends Lint.RuleWalker {
 
     protected visitBinaryExpression(node: ts.BinaryExpression): void {
-        if (this.isExpressionNaN(node.left) || this.isExpressionNaN(node.right)) {
+        if (this.isExpressionNaN(node.left) || this.isExpressionNaN(node.right)
+                && node.operatorToken.kind !== ts.SyntaxKind.EqualsToken) {
             this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING + node.getText()));
         }
         super.visitBinaryExpression(node);
