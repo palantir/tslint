eba8fa6c02fd297fb8ea7284e6c04da7fd846742
Stephen Spencer <stephen-git@vitaminj.co.uk>
2016-05-06 16:41:06 +0100

    Fixes line disabling flags from disabling max-line-length also on the following line (#1217)

--- src/enableDisableRules.ts
+++ src/enableDisableRules.ts
@@ -81,7 +81,7 @@ export class EnableDisableRulesWalker extends SkippableTokenAwareRuleWalker {
                         // end at the beginning of the next line
                         this.enableDisableRuleMap[ruleToAdd].push({
                             isEnabled: !isEnabled,
-                            position: scanner.getTextPos() + 1,
+                            position: scanner.getTextPos(),
                         });
                     } else {
                         // start at the current position
@@ -93,7 +93,7 @@ export class EnableDisableRulesWalker extends SkippableTokenAwareRuleWalker {
                         if (isNextLine) {
                             this.enableDisableRuleMap[ruleToAdd].push({
                                 isEnabled: !isEnabled,
-                                position: this.getStartOfLinePosition(node, startingPosition, 2),
+                                position: this.getStartOfLinePosition(node, startingPosition, 2) - 1,
                             });
                         }
                     }
