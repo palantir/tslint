f6e70381be76740a9802a11501403fd8cd4123a5
Jason Killian <jkillian@palantir.com>
2016-06-20 15:10:54 -0400

    Remove unecessary whitespace from metadata

--- src/rules/jsdocFormatRule.ts
+++ src/rules/jsdocFormatRule.ts
@@ -25,7 +25,7 @@ export class Rule extends Lint.Rules.AbstractRule {
         description: "Enforces basic format rules for JSDoc comments.",
         descriptionDetails: Lint.Utils.dedent`
             The following rules are enforced for JSDoc comments (comments starting with \`/**\`):
-             
+
             * each line contains an asterisk and asterisks must be aligned
             * each asterisk must be followed by either a space or a newline (except for the first and the last)
             * the only characters before the asterisk on each line must be whitespace characters
