745f353bf28a3febd2b4477f010b40a4e1f44f84
Dan Vanderkam <danvk@sidewalklabs.com>
2016-08-16 12:42:20 -0400

    object-shorthand rule

---
+++ src/rules/objectShorthandRule.ts
@@ -0,0 +1,44 @@
+import * as Lint from "../lint";
+import * as ts from "typescript";
+
+export class Rule extends Lint.Rules.AbstractRule {
+    /* tslint:disable:object-literal-sort-keys */
+    public static metadata: Lint.IRuleMetadata = {
+        ruleName: "object-shorthand",
+        description: "Enforces use of ES6 object literal shorthand.",
+        descriptionDetails: Lint.Utils.dedent``,
+        optionsDescription: Lint.Utils.dedent``,
+        options: null,
+        optionExamples: ["true"],
+        type: "style",
+    };
+    /* tslint:enable:object-literal-sort-keys */
+
+    public static LONGHAND_PROPERTY = "Expected property shorthand.";
+    public static LONGHAND_METHOD = "Expected method shorthand.";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        const objectShorthandWalker = new ObjectShorthandWalker(sourceFile, this.getOptions());
+        return this.applyWithWalker(objectShorthandWalker);
+    }
+}
+
+class ObjectShorthandWalker extends Lint.RuleWalker {
+
+    public visitPropertyAssignment(node: ts.PropertyAssignment) {
+        const name = node.name;
+        const value = node.initializer;
+
+        if (name.kind === ts.SyntaxKind.Identifier &&
+            value.kind === ts.SyntaxKind.Identifier &&
+            name.getText() === value.getText()) {
+                this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.LONGHAND_PROPERTY));
+        }
+
+        if (value.kind === ts.SyntaxKind.FunctionExpression) {
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.LONGHAND_METHOD));
+        }
+
+        super.visitPropertyAssignment(node);
+    }
+}
