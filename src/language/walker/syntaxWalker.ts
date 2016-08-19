a3d4f3eb3d50481b9f1b2475c37ca21208d776c3
ScottSWu <sswu@google.com>
2016-08-19 11:28:25 -0700

    Add ArrayType rule to enforce 'T[]' or 'Array<T>'
    * Add ArrayType to SyntaxWalker

--- src/language/walker/syntaxWalker.ts
+++ src/language/walker/syntaxWalker.ts
@@ -30,6 +30,10 @@ export class SyntaxWalker {
         this.walkChildren(node);
     }
 
+    protected visitArrayType(node: ts.ArrayTypeNode) {
+        this.walkChildren(node);
+    }
+
     protected visitArrowFunction(node: ts.FunctionLikeDeclaration) {
         this.walkChildren(node);
     }
@@ -326,6 +330,10 @@ export class SyntaxWalker {
 
             case ts.SyntaxKind.ArrayLiteralExpression:
                 this.visitArrayLiteralExpression(<ts.ArrayLiteralExpression> node);
+                break;
+
+            case ts.SyntaxKind.ArrayType:
+                this.visitArrayType(<ts.ArrayTypeNode> node);
                 break;
 
             case ts.SyntaxKind.ArrowFunction:
