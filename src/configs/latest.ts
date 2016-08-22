d6779ddb31048a394779a5d3f82a359b64edbca3
Adi Dahiya <adi.dahiya14@gmail.com>
2016-08-22 15:40:47 -0400

    v3.15.1 with updated tslint:latest config

--- src/configs/latest.ts
+++ src/configs/latest.ts
@@ -16,8 +16,14 @@
  */
 
 export const rules = {
+    "adjacent-overload-signatures": true,
+    "no-for-in-array": true,
     "no-unsafe-finally": true,
+    "object-literal-key-quotes": [true, "as-needed"],
+    "object-literal-shorthand": true,
+    "only-arrow-functions": [true, "allow-declarations"],
     "ordered-imports": [true, {
+        "import-sources-order": "case-insensitive",
         "named-imports-order": "lowercase-last",
     }],
 };
