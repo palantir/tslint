138405243b4d3169be488cbd8ef71afe71fa8e89
Boris Aranovich <nomaed@gmail.com>
2016-09-18 22:16:10 +0200

    Added test for filelist formatter

---
+++ test/files/formatters/filelistFormatter.test.ts
@@ -0,0 +1,9 @@
+module FileListModule {
+    export class FileListClass {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+    }
+}
---
+++ test/formatters/filelistFormatterTests.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as ts from "typescript";
+
+import {IFormatter, RuleFailure, TestUtils} from "../lint";
+
+describe("File-list Formatter", () => {
+    const TEST_FILE = "formatters/filelistFormatter.test.ts";
+    let sourceFile: ts.SourceFile;
+    let formatter: IFormatter;
+
+    before(() => {
+        const Formatter = TestUtils.getFormatter("filelist");
+        sourceFile = TestUtils.getSourceFile(TEST_FILE);
+        formatter = new Formatter();
+    });
+
+    it("formats failures", () => {
+        // this part really doesn't matter, as long as we get some failure`
+        const failures = [
+            new RuleFailure(sourceFile, 0, 1, "first failure", "first-name"),
+            new RuleFailure(sourceFile, 32, 36, "last failure", "last-name"),
+        ];
+
+        // we only print file-names in this formatter
+        const expectedResult = TEST_FILE + "\n";
+
+        const actualResult = formatter.format(failures);
+        assert.equal(actualResult, expectedResult);
+    });
+
+    it("handles no failures", () => {
+        const result = formatter.format([]);
+        assert.equal(result, "\n");
+    });
+});
