6979cdaec50a0bc8e82db90b55052a52238e9b7d
Simon Schick <SimonSchick@users.noreply.github.com>
2016-12-13 04:26:17 +0100

    New rule: import-blacklist (#1841)
    This rule checks for require/import statements with a user-specified list of banned strings. This is useful to prevent accidental inclusions of entire libraries, when tree shaking is not possible. E.g. importing "material-ui" instead of "material-ui/lib/something".
    
    Contributors: @use-strict

--- src/formatters/codeFrameFormatter.ts
+++ src/formatters/codeFrameFormatter.ts
@@ -17,7 +17,7 @@
 
 import {AbstractFormatter} from "../language/formatter/abstractFormatter";
 import {IFormatterMetadata} from "../language/formatter/formatter";
-import {RuleFailure} from "../language/rule/rule";
+import { RuleFailure } from "../language/rule/rule";
 
 import codeFrame = require("babel-code-frame");
 import * as colors from "colors";
