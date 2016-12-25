e9be54753845d3d31b38e1e342afe7663ad81cc9
Mohsen Azimi <mazimi@lyft.com>
2016-12-25 14:20:27 -0800

    Enforce space after import

--- src/rules/importSpacingRule.ts
+++ src/rules/importSpacingRule.ts
@@ -31,6 +31,11 @@ export class Rule extends Lint.Rules.AbstractRule {
         typescriptOnly: false,
     };
 
+    public static TOO_MANY_SPACES_AFTER_IMPORT = "Too many spaces after import";
+    public static ADD_SPACE_AFTER_IMPORT = "Add space after import";
+    public static TOO_MANY_SPACES_AFTER_STAR = "Too many spaces after *";
+    public static ADD_SPACE_AFTER_STAR = "Add space after *";
+
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         const comparisonWalker = new ImportStatementWalker(sourceFile, this.getOptions());
         return this.applyWithWalker(comparisonWalker);
@@ -38,16 +43,25 @@ export class Rule extends Lint.Rules.AbstractRule {
 }
 
 class ImportStatementWalker extends Lint.RuleWalker {
+    private static IMPORT_KEYWORD_LENGTH = "import".length;
+
     public visitImportDeclaration(node: ts.ImportDeclaration) {
+        const importKeywordEnd = node.getStart() + ImportStatementWalker.IMPORT_KEYWORD_LENGTH;
+        const importClauseStart = node.importClause.getStart();
+        if (importKeywordEnd === importClauseStart) {
+            this.addFailure(this.createFailure(node.getStart(), ImportStatementWalker.IMPORT_KEYWORD_LENGTH, Rule.ADD_SPACE_AFTER_IMPORT));
+        } else if (importClauseStart > (importKeywordEnd + 1)) {
+            this.addFailure(this.createFailure(node.getStart(), importClauseStart - node.getStart(), Rule.TOO_MANY_SPACES_AFTER_IMPORT));
+        }
         super.visitImportDeclaration(node);
     }
 
     public visitNamespaceImport(node: ts.NamespaceImport) {
         const text = node.getText();
         if (text.indexOf("*as") > -1) {
-            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), "Add space after *"));
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.ADD_SPACE_AFTER_STAR));
         } else if (/\*\s{2,}as/.test(text)) {
-            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), "Too many spaces after *"));
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.TOO_MANY_SPACES_AFTER_STAR));
         }
         super.visitNamespaceImport(node);
     }
