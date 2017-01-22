821f65d41978485ae59b459f5b0bc091e54c1a42
heycalmdown <heyacct@gmail.com>
2017-01-22 13:53:30 +0900

    Add support to fix `align` rule

--- src/rules/alignRule.ts
+++ src/rules/alignRule.ts
@@ -106,8 +106,14 @@ class AlignWalker extends Lint.RuleWalker {
         for (const node of nodes.slice(1)) {
             const curPos = this.getLineAndCharacterOfPosition(node.getStart());
             if (curPos.line !== prevPos.line && curPos.character !== alignToColumn) {
-                this.addFailureAtNode(node, kind + Rule.FAILURE_STRING_SUFFIX);
-                break;
+                const diff = alignToColumn - curPos.character;
+                let fix;
+                if (0 < diff) {
+                    fix = this.createFix(this.appendText(node.getStart(), " ".repeat(diff)));
+                } else {
+                    fix = this.createFix(this.deleteText(node.getStart() + diff, -diff));
+                }
+                this.addFailureAtNode(node, kind + Rule.FAILURE_STRING_SUFFIX, fix);
             }
             prevPos = curPos;
         }
