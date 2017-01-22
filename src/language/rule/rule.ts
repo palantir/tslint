23a15653c40a4e556ab709a5777e0a58d3543ecb
Josh Goldberg <joshuakgoldberg@outlook.com>
2017-01-21 20:21:29 -0800

    Added interface descriptors to Replacement (#2092)
    Fixes https://github.com/palantir/tslint/issues/1788.

--- src/language/rule/rule.ts
+++ src/language/rule/rule.ts
@@ -104,6 +104,21 @@ export interface IRule {
     applyWithWalker(walker: IWalker): RuleFailure[];
 }
 
+export interface IRuleFailureJson {
+    endPosition: IRuleFailurePositionJson;
+    failure: string;
+    fix?: Fix;
+    name: string;
+    ruleName: string;
+    startPosition: IRuleFailurePositionJson;
+}
+
+export interface IRuleFailurePositionJson {
+    character: number;
+    line: number;
+    position: number;
+}
+
 export class Replacement {
     public static applyAll(content: string, replacements: Replacement[]) {
         // sort in reverse so that diffs are properly applied
@@ -173,7 +188,7 @@ export class RuleFailurePosition {
         return this.lineAndCharacter;
     }
 
-    public toJson() {
+    public toJson(): IRuleFailurePositionJson {
         return {
             character: this.lineAndCharacter.character,
             line: this.lineAndCharacter.line,
@@ -242,7 +257,7 @@ export class RuleFailure {
         return this.rawLines;
     }
 
-    public toJson(): any {
+    public toJson(): IRuleFailureJson {
         return {
             endPosition: this.endPosition.toJson(),
             failure: this.failure,
