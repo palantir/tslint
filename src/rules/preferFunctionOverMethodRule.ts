1a2c3b9c8c68338bf44022f9fdee5c1ef1bdb791
Adi Dahiya <adi.dahiya14@gmail.com>
2017-01-25 12:03:26 -0500

    Clean up preferFunctionOverMethodRule
    More descriptive failure message, minor code style fixes

--- src/rules/preferFunctionOverMethodRule.ts
+++ src/rules/preferFunctionOverMethodRule.ts
@@ -26,10 +26,10 @@ export class Rule extends Lint.Rules.AbstractRule {
     /* tslint:disable:object-literal-sort-keys */
     public static metadata: Lint.IRuleMetadata = {
         ruleName: "prefer-function-over-method",
-        description: "Warns for methods that do not use 'this'.",
+        description: "Warns for class methods that do not use 'this'.",
         optionsDescription: Lint.Utils.dedent`
-            "${OPTION_ALLOW_PUBLIC}" excludes public methods.
-            "${OPTION_ALLOW_PROTECTED}" excludes protected methods.`,
+            "${OPTION_ALLOW_PUBLIC}" excludes checking of public methods.
+            "${OPTION_ALLOW_PROTECTED}" excludes checking of protected methods.`,
         options: {
             type: "string",
             enum: [OPTION_ALLOW_PUBLIC, OPTION_ALLOW_PROTECTED],
@@ -43,14 +43,14 @@ export class Rule extends Lint.Rules.AbstractRule {
     };
     /* tslint:enable:object-literal-sort-keys */
 
-    public static FAILURE_STRING = "Method does not use 'this'. Use a function instead.";
+    public static FAILURE_STRING = "Class method does not use 'this'. Use a function instead.";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithWalker(new Walker(sourceFile, this.getOptions()));
+        return this.applyWithWalker(new PreferFunctionOverMethodWalker(sourceFile, this.getOptions()));
     }
 }
 
-class Walker extends Lint.RuleWalker {
+class PreferFunctionOverMethodWalker extends Lint.RuleWalker {
     private allowPublic = this.hasOption(OPTION_ALLOW_PUBLIC);
     private allowProtected = this.hasOption(OPTION_ALLOW_PROTECTED);
     private stack: ThisUsed[] = [];
@@ -66,10 +66,11 @@ class Walker extends Lint.RuleWalker {
                 const { name } = node as ts.MethodDeclaration;
                 const usesThis = this.withThisScope(
                     name.kind === ts.SyntaxKind.Identifier ? name.text : undefined,
-                    () => super.visitNode(node));
-                if (!usesThis &&
-                    node.parent!.kind !== ts.SyntaxKind.ObjectLiteralExpression &&
-                    this.shouldWarnForModifiers(node as ts.MethodDeclaration)) {
+                    () => super.visitNode(node),
+                );
+                if (!usesThis
+                        && node.parent!.kind !== ts.SyntaxKind.ObjectLiteralExpression
+                        && this.shouldWarnForModifiers(node as ts.MethodDeclaration)) {
                     this.addFailureAtNode((node as ts.MethodDeclaration).name, Rule.FAILURE_STRING);
                 }
                 break;
@@ -114,19 +115,26 @@ class Walker extends Lint.RuleWalker {
     }
 }
 
-interface ThisUsed { readonly name: string | undefined; isThisUsed: boolean; }
+interface ThisUsed {
+    readonly name: string | undefined;
+    isThisUsed: boolean;
+}
 
 function isRecursiveCall(thisOrSuper: ts.Node, cur: ThisUsed) {
     const parent = thisOrSuper.parent!;
-    return thisOrSuper.kind === ts.SyntaxKind.ThisKeyword &&
-        parent.kind === ts.SyntaxKind.PropertyAccessExpression &&
-        (parent as ts.PropertyAccessExpression).name.text === cur.name;
+    return thisOrSuper.kind === ts.SyntaxKind.ThisKeyword
+        && parent.kind === ts.SyntaxKind.PropertyAccessExpression
+        && (parent as ts.PropertyAccessExpression).name.text === cur.name;
 }
 
 const enum Visibility { Public, Protected, Private }
 
 function methodVisibility(node: ts.MethodDeclaration): Visibility {
-    return Lint.hasModifier(node.modifiers, ts.SyntaxKind.PrivateKeyword) ? Visibility.Private :
-        Lint.hasModifier(node.modifiers, ts.SyntaxKind.ProtectedKeyword) ? Visibility.Protected :
-        Visibility.Public;
+    if (Lint.hasModifier(node.modifiers, ts.SyntaxKind.PrivateKeyword)) {
+        return Visibility.Private;
+    } else if (Lint.hasModifier(node.modifiers, ts.SyntaxKind.ProtectedKeyword)) {
+        return Visibility.Protected;
+    } else {
+        return Visibility.Protected;
+    }
 }
