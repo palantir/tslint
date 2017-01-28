4903c454b1e5f7b4f7321f1bcadecc9d0b5815d8
Ian Ker-Seymer <i.kerseymer@gmail.com>
2017-01-27 19:13:48 -0500

    f

--- test/formatters/fixFormatterTests.ts
+++ test/formatters/fixFormatterTests.ts
@@ -1,5 +1,6 @@
-/*
- * Copyright 2013 Palantir Technologies, Inc.
+/**
+ * @license
+ * Copyright 2017 Palantir Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
