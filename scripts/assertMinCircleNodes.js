8c13b523077c365640e818473e5ba8b1ecc3f214
Noah Chen <noahchen@gmail.com>
2017-03-02 13:38:36 -0500

    Update circle.yml to test against typescript@next (#2288)

--- scripts/assertMinCircleNodes.js
+++ scripts/assertMinCircleNodes.js
@@ -1,7 +1,7 @@
-var requiredNodes = 4;
+var requiredNodes = 5;
 var nodes = parseInt(process.argv[2], 10);
 if (requiredNodes != null && requiredNodes > nodes) {
-    console.error("ERROR: You must run CircleCI with 4 parallel nodes");
+    console.error("ERROR: You must run CircleCI with " + requiredNodes + " parallel nodes");
     console.error("    This ensures that different environments are tested for TSLint compatibility");
     console.error("    https://circleci.com/gh/<YOUR ACCOUNT>/tslint/edit#parallel-builds");
     process.exit(1);
