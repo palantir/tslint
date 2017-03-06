841c9c710baab7ca84e49f8ba24498925c83eadd
Josh Goldberg <joshuakgoldberg@outlook.com>
2017-03-06 09:23:45 -0800

    Added an abstract OptionallyTypedRule (#2300)

--- src/language/rule/optionallyTypedRule.ts
+++ src/language/rule/optionallyTypedRule.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2016 Palantir Technologies, Inc.
+ * Copyright 2017 Palantir Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,12 +20,7 @@ import * as ts from "typescript";
 import {AbstractRule} from "./abstractRule";
 import {ITypedRule, RuleFailure} from "./rule";
 
-export abstract class TypedRule extends AbstractRule implements ITypedRule {
-
-    public apply(): RuleFailure[] {
-        // if no program is given to the linter, throw an error
-        throw new Error(`The '${this.ruleName}' rule requires type checking`);
-    }
+export abstract class OptionallyTypedRule extends AbstractRule implements ITypedRule {
 
     public abstract applyWithProgram(sourceFile: ts.SourceFile, languageService: ts.LanguageService): RuleFailure[];
 }
