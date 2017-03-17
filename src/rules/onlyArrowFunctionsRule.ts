1a6e20ecb009a311cb0f3e74e22c6164eedbf476
Andy Hanson <andy.pj.hanson@gmail.com>
2017-03-16 17:58:30 -0700

    only-arrow-functions: Convert to function

--- src/rules/onlyArrowFunctionsRule.ts
+++ src/rules/onlyArrowFunctionsRule.ts
@@ -52,39 +52,43 @@ export class Rule extends Lint.Rules.AbstractRule {
     public static FAILURE_STRING = "non-arrow functions are forbidden";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithWalker(new OnlyArrowFunctionsWalker(sourceFile, this.getOptions()));
+        return this.applyWithFunction(sourceFile, walk, parseOptions(this.ruleArguments));
     }
 }
 
-class OnlyArrowFunctionsWalker extends Lint.RuleWalker {
-    private allowDeclarations: boolean;
-    private allowNamedFunctions: boolean;
+interface Options {
+    allowDeclarations: boolean;
+    allowNamedFunctions: boolean;
+}
+function parseOptions(ruleArguments: string[]): Options {
+    return {
+        allowDeclarations: hasOption(OPTION_ALLOW_DECLARATIONS),
+        allowNamedFunctions: hasOption(OPTION_ALLOW_NAMED_FUNCTIONS),
+    };
 
-    constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
-        super(sourceFile, options);
-        this.allowDeclarations = this.hasOption(OPTION_ALLOW_DECLARATIONS);
-        this.allowNamedFunctions = this.hasOption(OPTION_ALLOW_NAMED_FUNCTIONS);
+    function hasOption(name: string): boolean {
+        return ruleArguments.indexOf(name) !== -1;
     }
+}
 
-    public visitFunctionDeclaration(node: ts.FunctionDeclaration) {
-        if (!this.allowDeclarations && !this.allowNamedFunctions) {
-            this.failUnlessExempt(node);
+function walk(ctx: Lint.WalkContext<Options>): void {
+    const { sourceFile, options: { allowDeclarations, allowNamedFunctions } } = ctx;
+    ts.forEachChild(sourceFile, function cb(node: ts.Node): void {
+        switch (node.kind) {
+            case ts.SyntaxKind.FunctionDeclaration:
+                if (allowDeclarations) {
+                    break;
+                }
+                // falls through
+            case ts.SyntaxKind.FunctionExpression: {
+                const f = node as ts.FunctionLikeDeclaration;
+                if (!(allowNamedFunctions && f.name) && !functionIsExempt(f)) {
+                    ctx.addFailureAtNode(Lint.childOfKind(node, ts.SyntaxKind.FunctionKeyword)!, Rule.FAILURE_STRING);
+                }
+            }
         }
-        super.visitFunctionDeclaration(node);
-    }
-
-    public visitFunctionExpression(node: ts.FunctionExpression) {
-        if (node.name === undefined || !this.allowNamedFunctions) {
-            this.failUnlessExempt(node);
-        }
-        super.visitFunctionExpression(node);
-    }
-
-    private failUnlessExempt(node: ts.FunctionLikeDeclaration) {
-        if (!functionIsExempt(node)) {
-            this.addFailureAtNode(Lint.childOfKind(node, ts.SyntaxKind.FunctionKeyword)!, Rule.FAILURE_STRING);
-        }
-    }
+        ts.forEachChild(node, cb);
+    });
 }
 
 /** Generator functions and functions using `this` are allowed. */
