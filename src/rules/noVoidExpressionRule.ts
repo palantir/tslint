e6f36260ff76d5ad7bc35f3be81f5aa2c7a8b6a0
Andy Hanson <andy.pj.hanson@gmail.com>
2017-04-02 08:25:36 -0700

    Fix compile error

--- src/rules/noVoidExpressionRule.ts
+++ src/rules/noVoidExpressionRule.ts
@@ -49,7 +49,8 @@ export class Rule extends Lint.Rules.TypedRule {
 
     public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
         const ignoreArrowFunctionShorthand = this.ruleArguments.indexOf(OPTION_IGNORE_ARROW_FUNCTION_SHORTHAND) !== -1;
-        return this.applyWithFunction(sourceFile, (ctx) => walk(ctx, program.getTypeChecker()), { ignoreArrowFunctionShorthand });
+        return this.applyWithFunction<Options, Options>(
+            sourceFile, (ctx) => walk(ctx, program.getTypeChecker()), { ignoreArrowFunctionShorthand });
     }
 }
 
