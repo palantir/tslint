50fa32b8c5aabc9d99ba16d4d873811de4fed3ee
Alex Eagle <alexeagle@google.com>
2017-04-07 14:34:41 -0700

    add new rule to all config

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -162,6 +162,7 @@ export const rules = {
     ],
     "completed-docs": true,
     // "file-header": No sensible default
+    "deprecation": true,
     "import-spacing": true,
     "interface-name": true,
     "interface-over-type-literal": true,
