7c1c8e54df403fbcf139b5ffcb5e270af62aa77d
Andy Hanson <andy.pj.hanson@gmail.com>
2017-04-15 15:02:16 -0700

    Fix various `return-undefined` bugs

--- src/rules/returnUndefinedRule.ts
+++ src/rules/returnUndefinedRule.ts
@@ -45,17 +45,14 @@ export class Rule extends Lint.Rules.TypedRule {
 }
 
 function walk(ctx: Lint.WalkContext<void>, checker: ts.TypeChecker) {
-    return ts.forEachChild(ctx.sourceFile, cb);
-    function cb(node: ts.Node): void {
-        check(node);
-        return ts.forEachChild(node, cb);
-    }
-
-    function check(node: ts.Node): void {
-        if (!u.isReturnStatement(node)) {
-            return;
+    return ts.forEachChild(ctx.sourceFile, function cb(node: ts.Node): void {
+        if (u.isReturnStatement(node)) {
+            check(node);
         }
+        return ts.forEachChild(node, cb);
+    });
 
+    function check(node: ts.ReturnStatement): void {
         const actualReturnKind = returnKindFromReturn(node);
         if (actualReturnKind === undefined) {
             return;
@@ -106,27 +103,38 @@ function getReturnKind(node: FunctionLike, checker: ts.TypeChecker): ReturnKind 
             return ReturnKind.Void;
         case ts.SyntaxKind.GetAccessor:
             return ReturnKind.Value;
-        default:
     }
 
-    // Go with an explicit type declaration if possible.
-    if (node.type) {
-        return node.type.kind === ts.SyntaxKind.VoidKeyword ? ReturnKind.Void : ReturnKind.Value;
-    }
+    const contextual = isFunctionExpressionLike(node) ? tryGetReturnType(checker.getContextualType(node)) : undefined;
+    const returnType = contextual !== undefined ? contextual : tryGetReturnType(checker.getTypeAtLocation(node));
 
-    const contextualType = node.kind === ts.SyntaxKind.FunctionExpression || node.kind === ts.SyntaxKind.ArrowFunction
-        ? checker.getContextualType(node)
-        : undefined;
-
-    const ty = contextualType || checker.getTypeAtLocation(node);
-    if (!ty) {
-        // Type error somewhere. Bail.
+    if (returnType === undefined) {
         return undefined;
+    } else if (Lint.isTypeFlagSet(returnType, ts.TypeFlags.Void)) {
+        return ReturnKind.Void;
+    } else if (Lint.hasModifier(node.modifiers, ts.SyntaxKind.AsyncKeyword)) {
+        // TODO: Would need access to `checker.getPromisedTypeOfPromise` to do this properly.
+        // Assume that the return type is the global Promise (since this is an async function) and get its type argument.
+        const typeArguments = (returnType as ts.GenericType).typeArguments;
+        if (typeArguments !== undefined && typeArguments.length === 1) {
+            return Lint.isTypeFlagSet(typeArguments[0], ts.TypeFlags.Void) ? ReturnKind.Void : ReturnKind.Value;
+        }
     }
+    return ReturnKind.Value;
 
-    const sig = checker.getSignaturesOfType(ty, ts.SignatureKind.Call)[0];
-    const returnType = checker.getReturnTypeOfSignature(sig);
-    return Lint.isTypeFlagSet(returnType, ts.TypeFlags.Void) ? ReturnKind.Void : ReturnKind.Value;
+    function tryGetReturnType(fnType: ts.Type | undefined): ts.Type | undefined {
+        if (fnType === undefined) {
+            return undefined;
+        }
+
+        const sigs = checker.getSignaturesOfType(fnType, ts.SignatureKind.Call);
+        if (sigs.length !== 1) {
+            return undefined;
+        }
+
+        const ret = checker.getReturnTypeOfSignature(sigs[0]);
+        return Lint.isTypeFlagSet(ret, ts.TypeFlags.Any) ? undefined : ret;
+    }
 }
 
 function isFunctionLike(node: ts.Node): node is FunctionLike {
@@ -138,6 +146,16 @@ function isFunctionLike(node: ts.Node): node is FunctionLike {
         case ts.SyntaxKind.Constructor:
         case ts.SyntaxKind.GetAccessor:
         case ts.SyntaxKind.SetAccessor:
+            return true;
+        default:
+            return false;
+    }
+}
+
+function isFunctionExpressionLike(node: ts.Node): node is ts.FunctionExpression | ts.ArrowFunction {
+    switch (node.kind) {
+        case ts.SyntaxKind.FunctionExpression:
+        case ts.SyntaxKind.ArrowFunction:
             return true;
         default:
             return false;
