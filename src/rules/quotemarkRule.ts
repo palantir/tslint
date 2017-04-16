4061b66ea5ae951770981dbb42c4b56b69575579
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-04-16 23:00:44 +0200

    object-literal-sort-keys: rewrite, handle spread and shorthand
    [new-rule-option] `object-literal-sort-keys` adds `ignore-case`
    Fixes: #1924
    [enhancement] `object-literal-sort-keys`: check shorthand properties
    Fixes: #2205
    [bugfix] `object-literal-sort-keys`: handle object spread correctly
    Fixes: #2554

--- src/rules/quotemarkRule.ts
+++ src/rules/quotemarkRule.ts
@@ -77,11 +77,11 @@ export class Rule extends Lint.Rules.AbstractRule {
         const args = this.ruleArguments;
         const quoteMark = args[0] === OPTION_SINGLE ? "'" : '"';
         return this.applyWithFunction(sourceFile, walk, {
-            quoteMark,
             avoidEscape: args.indexOf(OPTION_AVOID_ESCAPE) !== -1,
             jsxQuoteMark: args.indexOf(OPTION_JSX_SINGLE) !== -1
                           ? "'"
                           : args.indexOf(OPTION_JSX_DOUBLE) ? '"' : quoteMark,
+            quoteMark,
         });
     }
 }
