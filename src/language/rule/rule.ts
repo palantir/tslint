f0e6d312ef01488b530d15c3d768e7604dcf97de
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-05-19 05:55:42 +0200

    Fix lint errors on master (#2781)

--- src/language/rule/rule.ts
+++ src/language/rule/rule.ts
@@ -159,7 +159,7 @@ export class Replacement {
 
     public static applyAll(content: string, replacements: Replacement[]) {
         // sort in reverse so that diffs are properly applied
-        replacements.sort((a, b) => b.end - a.end || b.start - a.start);
+        replacements.sort((a, b) => b.end !== a.end ? b.end - a.end : b.start - a.start);
         return replacements.reduce((text, r) => r.apply(text), content);
     }
 
