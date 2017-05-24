6a7197c0257cbc55d0d51a4cb3703d679839ba70
Andy Hanson <andy.pj.hanson@gmail.com>
2017-05-23 18:48:43 -0700

    Add "type-alias-delimiter" option; default to ","

--- src/rules/typeLiteralDelimiterRule.ts
+++ src/rules/typeLiteralDelimiterRule.ts
@@ -20,40 +20,71 @@ import * as ts from "typescript";
 
 import * as Lint from "../index";
 
+const OPTION_TYPE_ALIAS_DELIMITER: keyof Options = "type-alias-delimiter";
+
 export class Rule extends Lint.Rules.AbstractRule {
     /* tslint:disable:object-literal-sort-keys */
     public static metadata: Lint.IRuleMetadata = {
         ruleName: "type-literal-delimiter",
         description: Lint.Utils.dedent`
-            Checks that single-line type literal members are separated by commas.
-            Checks that multi-line type literal members are separated by semicolons.
-            Enforces a trailing semicolon for multi-line type literals.`,
-        optionsDescription: "Not configurable.",
-        options: null,
-        optionExamples: [true],
+            Checks that type literal members are separated by commas.
+            Enforces a trailing delimiter for multi-line type literals.`,
+        optionsDescription: Lint.Utils.dedent`
+            An options object may be passed with the following keys:
+
+            * '${OPTION_TYPE_ALIAS_DELIMITER}': This may be set to one of:
+              - \`","\`: This is the default. Type aliases are treated the same as other type literals.
+              - \`";"\`: Use semicolons for type aliases instead of commas.
+              - \`"ignore\`: Do not check the delimiter used by a type alias. (Still check for a trailing delimiter.)`,
+        options: {
+            type: "object",
+            properties: {
+                [OPTION_TYPE_ALIAS_DELIMITER]: {
+                    enum: [
+                        ",",
+                        ";",
+                        "ignore",
+                    ],
+                    type: "string",
+                },
+            },
+        },
+        optionExamples: [
+            true,
+            [true, { [OPTION_TYPE_ALIAS_DELIMITER]: ";" }],
+            [true, { [OPTION_TYPE_ALIAS_DELIMITER]: "ignore" }],
+        ],
         type: "style",
         typescriptOnly: true,
     };
     /* tslint:enable:object-literal-sort-keys */
 
-    public static FAILURE_STRING_MISSING =
-        "Expected multi-line type literal to have a trailing ';'.";
-    public static FAILURE_STRING_PREFER_COMMA =
-        "Expected multi-line type literal to use ';' instead of ','.";
-    public static FAILURE_STRING_PREFER_SEMICOLON =
-        "Expected single-line type literal to use ',' instead of ';'.";
+    public static FAILURE_STRING_PREFER(delimiter: string): string {
+        return `Expected type literal to use '${delimiter}' as a delimiter.`;
+    }
+    public static FAILURE_STRING_MISSING(delimiter: string): string {
+        return `Expected multi-line type literal to have a trailing '${delimiter}'.`;
+    }
     public static FAILURE_STRING_TRAILING(delimiter: string): string {
         return `Did not expect single-line type literal to have a trailing '${delimiter}'.`;
     }
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithFunction(sourceFile, walk);
+        const options: Options = {
+            "type-alias-delimiter": ",",
+            ...(this.ruleArguments[0] as Options | undefined),
+        };
+        return this.applyWithFunction(sourceFile, walk, options);
     }
 }
 
-function walk(ctx: Lint.WalkContext<void>): void {
-    const { sourceFile } = ctx;
-    ts.forEachChild(sourceFile, function cb(node: ts.Node): void {
+interface Options {
+    "type-alias-delimiter": "," | ";" | "ignore";
+}
+
+function walk(ctx: Lint.WalkContext<Options>): void {
+    const { sourceFile, options: { "type-alias-delimiter": typeAliasDelimiter } } = ctx;
+    ts.forEachChild(sourceFile, function cb(node) {
         if (isTypeLiteralNode(node)) {
             check(node);
         }
@@ -61,22 +92,23 @@ function walk(ctx: Lint.WalkContext<void>): void {
     });
 
     function check(node: ts.TypeLiteralNode): void {
-        const preferComma = isSameLine(sourceFile, node.getStart(sourceFile), node.getEnd());
+        const preferred = node.parent!.kind === ts.SyntaxKind.TypeAliasDeclaration ? typeAliasDelimiter : ",";
+        const singleLine = isSameLine(sourceFile, node.getStart(sourceFile), node.getEnd());
         node.members.forEach((member, idx) => {
             const end = member.end - 1;
             const delimiter = sourceFile.text[end];
             const anyDelimiter = delimiter === ";" || delimiter === ",";
             // Trailing delimiter should be ommitted for a single-line type literal.
-            if (preferComma && idx === node.members.length - 1) {
+            if (singleLine && idx === node.members.length - 1) {
                 if (anyDelimiter) {
                     fail(Rule.FAILURE_STRING_TRAILING(delimiter));
                 }
             } else if (anyDelimiter) {
-                if ((delimiter === ",") !== preferComma) {
-                    fail(preferComma ? Rule.FAILURE_STRING_PREFER_SEMICOLON : Rule.FAILURE_STRING_PREFER_COMMA);
+                if (delimiter !== preferred && preferred !== "ignore") {
+                    fail(Rule.FAILURE_STRING_PREFER(preferred));
                 }
             } else {
-                fail(Rule.FAILURE_STRING_MISSING);
+                fail(Rule.FAILURE_STRING_MISSING(preferred));
             }
 
             function fail(failure: string): void {
