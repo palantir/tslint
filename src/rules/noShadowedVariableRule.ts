0fd3e95f1de63e2c71f2a902da81e743b9a7d32a
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-06-06 22:02:54 +0200

    Rephrase error message

--- src/rules/noShadowedVariableRule.ts
+++ src/rules/noShadowedVariableRule.ts
@@ -37,7 +37,7 @@ export class Rule extends Lint.Rules.AbstractRule {
     /* tslint:enable:object-literal-sort-keys */
 
     public static FAILURE_STRING_FACTORY(name: string) {
-        return `Shadowed variable: '${name}'`;
+        return `Shadowed name: '${name}'`;
     }
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
