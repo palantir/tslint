9ae9983ed6e6cc57995cea2ca0b7640bfbf6def3
Josh Goldberg <josh@fullscreenmario.com>
2017-06-15 14:03:58 -0700

    Added no-self-this to tslint:all
    No violations in the code base

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -51,6 +51,7 @@ export const rules = {
     "no-namespace": true,
     "no-non-null-assertion": true,
     "no-reference": true,
+    "no-self-this": true,
     "no-var-requires": true,
     "only-arrow-functions": true,
     "prefer-for-of": true,
