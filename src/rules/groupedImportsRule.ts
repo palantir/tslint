c3d74ccb1d530afe43e698c35e8667e68458124e
Erik Timmers <e.timmers@gmail.com>
2017-07-28 14:18:42 +0200

    Rewrite to always use a single fix
    And preserve all statements.

--- src/rules/groupedImportsRule.ts
+++ src/rules/groupedImportsRule.ts
@@ -34,10 +34,7 @@ export class Rule extends Lint.Rules.AbstractRule {
     };
     /* tslint:enable:object-literal-sort-keys */
 
-    public static IMPORT_SOURCES_SEPARATED = "Import sources within a group must not be separated by blank lines";
-    public static IMPORT_SOURCES_NOT_SEPARATED =
-        "Import sources of different groups must be separated by a single blank line";
-    public static IMPORT_SOURCES_ORDER =
+    public static GROUPED_IMPORTS =
         "Import sources of different groups must be sorted by: libraries, parent directories, current directory";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
@@ -52,49 +49,45 @@ enum ImportStatementType {
 }
 
 interface ImportStatement {
-    statement: ts.Statement;
+    statement: ts.ImportDeclaration;
     type: ImportStatementType;
     lineStart: number;
     lineEnd: number;
 }
 
 class Walker extends Lint.AbstractWalker<Lint.IOptions> {
-    private lastImportStatement: ImportStatement;
-    private newLine: string;
-    private allImportsFix: boolean;
+    private previousImportStatement: ImportStatement | undefined;
 
     public walk(sourceFile: ts.SourceFile): void {
-        this.newLine = this.getEofChar(sourceFile);
-        sourceFile.statements
+        const importsStatements = sourceFile.statements
             .filter(isImportDeclaration)
-            .forEach(this.checkStatement);
+            .map(this.toImportStatement);
+        const firstFailure = importsStatements.find(this.isBadlyPositioned);
+        if (firstFailure != null) {
+            const fix = this.createFix(importsStatements);
+            this.addFailureAtNode(firstFailure.statement, Rule.GROUPED_IMPORTS, fix);
+        }
     }
 
-    private getEofChar(sourceFile: ts.SourceFile): string {
-        const lineEnd = sourceFile.getLineEndOfPosition(0);
-        let newLine;
-        if (lineEnd > 0) {
-            if (lineEnd > 1 && sourceFile.text[lineEnd - 1] === "\r") {
-                newLine = "\r\n";
-            } else if (sourceFile.text[lineEnd] === "\n") {
-                newLine = "\n";
+    private isBadlyPositioned = (importStatement: ImportStatement): boolean => {
+        if (this.previousImportStatement != null) {
+            if (importStatement.type === this.previousImportStatement.type) {
+                if (importStatement.lineStart !== this.previousImportStatement.lineEnd + 1) {
+                    return true;
+                }
+            } else {
+                if (importStatement.type < this.previousImportStatement.type) {
+                    return true;
+                } else if (importStatement.lineStart !== this.previousImportStatement.lineEnd + 2) {
+                    return true;
+                }
             }
         }
-        return newLine == null ? ts.sys.newLine : newLine;
+        this.previousImportStatement = importStatement;
+        return false;
     }
 
-    private checkStatement = (statement: ts.ImportDeclaration): void => {
-        if (this.allImportsFix) {
-            return;
-        }
-        const importStatement = this.toImportStatement(statement);
-        if (this.lastImportStatement != null) {
-            this.checkForFailure(importStatement);
-        }
-        this.lastImportStatement = importStatement;
-    }
-
-    private toImportStatement(statement: ts.ImportDeclaration): ImportStatement {
+    private toImportStatement = (statement: ts.ImportDeclaration): ImportStatement => {
         return {
             lineEnd: this.sourceFile.getLineAndCharacterOfPosition(statement.getEnd()).line,
             lineStart: this.sourceFile.getLineAndCharacterOfPosition(statement.getStart()).line,
@@ -123,60 +116,56 @@ class Walker extends Lint.AbstractWalker<Lint.IOptions> {
         return "";
     }
 
-    private checkForFailure(importStatement: ImportStatement): void {
-        if (importStatement.type === this.lastImportStatement.type) {
-            if (importStatement.lineStart !== this.lastImportStatement.lineEnd + 1) {
-                this.addSeparatedImportsFailure(importStatement);
+    private createFix(importStatements: ImportStatement[]): Lint.Fix {
+        const newLine = this.getEofChar(this.sourceFile);
+        const imports = this.getOrderedImports(importStatements);
+        const addition = Lint.Replacement.appendText(0, imports.join(newLine));
+        const deletions = importStatements.map((imp) => {
+            const [start, end] = this.getRangeIncludingWhitespace(imp.statement);
+            return Lint.Replacement.deleteFromTo(start, end);
+        });
+        return [...deletions, addition];
+    }
+
+    private getOrderedImports(importStatements: ImportStatement[]): string[] {
+        const libs = importStatements.filter((imp) => imp.type === ImportStatementType.LIBRARY_IMPORT);
+        const parent = importStatements.filter((imp) => imp.type === ImportStatementType.PARENT_DIRECTORY_IMPORT);
+        const current = importStatements.filter((imp) => imp.type === ImportStatementType.CURRENT_DIRECTORY_IMPORT);
+        return [libs, parent, current].reduce((arr, imps) => {
+            if (imps.length == 0) {
+                return arr;
             }
-        } else {
-            if (importStatement.type < this.lastImportStatement.type) {
-                this.addIncorrectlyOrderedImportsFailure(importStatement);
-            } else if (importStatement.lineStart !== this.lastImportStatement.lineEnd + 2) {
-                this.addNotSeparatedImportsFailure(importStatement);
+            return arr.concat(imps.map((imp) => imp.statement.getText()), "");
+        }, [] as string[]).concat("");
+    }
+
+    private getRangeIncludingWhitespace(statement: ts.ImportDeclaration): [number, number] {
+        const text = this.sourceFile.text;
+        let start = statement.getStart();
+        while (this.isWhiteSpaceChar(text[start - 1])) {
+            start--;
+        }
+        let end = statement.getEnd();
+        while (this.isWhiteSpaceChar(text[end + 1])) {
+            end++;
+        }
+        return [start, end];
+    }
+
+    private isWhiteSpaceChar(char: string | undefined): boolean {
+        return char === undefined ? false : Lint.isWhiteSpace(char.charCodeAt(0));
+    }
+
+    private getEofChar(sourceFile: ts.SourceFile): string {
+        const lineEnd = sourceFile.getLineEndOfPosition(0);
+        let newLine;
+        if (lineEnd > 0) {
+            if (lineEnd > 1 && sourceFile.text[lineEnd - 1] === "\r") {
+                newLine = "\r\n";
+            } else if (sourceFile.text[lineEnd] === "\n") {
+                newLine = "\n";
             }
         }
-    }
-
-    private addSeparatedImportsFailure(importStatement: ImportStatement): void {
-        const text = [this.lastImportStatement, importStatement]
-            .map((st) => st.statement.getText())
-            .join(this.newLine);
-        const replacement = Lint.Replacement.replaceFromTo(
-            this.lastImportStatement.statement.getStart(), importStatement.statement.getEnd(), text);
-        this.addFailureAtNode(importStatement.statement, Rule.IMPORT_SOURCES_SEPARATED, replacement);
-    }
-
-    private addIncorrectlyOrderedImportsFailure(importStatement: ImportStatement): void {
-        this.allImportsFix = true;
-        this.failures.length = 0;
-        this.addFailureAtNode(importStatement.statement, Rule.IMPORT_SOURCES_ORDER, this.getAllImportsFix());
-    }
-
-    private addNotSeparatedImportsFailure(importStatement: ImportStatement): void {
-        const replacement = Lint.Replacement.replaceFromTo(this.lastImportStatement.statement.getEnd(),
-            importStatement.statement.getStart(), this.newLine + this.newLine);
-        this.addFailureAtNode(importStatement.statement, Rule.IMPORT_SOURCES_NOT_SEPARATED, replacement);
-    }
-
-    private getAllImportsFix(): Lint.Fix {
-        const importStatements = this.sourceFile.statements.filter(isImportDeclaration);
-        const libs = importStatements.filter(
-            (st) => this.getImportStatementType(st) === ImportStatementType.LIBRARY_IMPORT);
-        const parent = importStatements.filter(
-            (st) => this.getImportStatementType(st) === ImportStatementType.PARENT_DIRECTORY_IMPORT);
-        const current = importStatements.filter(
-            (st) => this.getImportStatementType(st) === ImportStatementType.CURRENT_DIRECTORY_IMPORT);
-        let imports: string[] = [];
-        [libs, parent, current].forEach((statements) => {
-            if (statements.length > 0) {
-                imports = imports.concat(statements.map((st) => st.getText()));
-                imports.push("");
-            }
-        });
-        return Lint.Replacement.replaceFromTo(
-            importStatements[0].getStart(),
-            importStatements[importStatements.length - 1].getEnd(),
-            imports.join(this.newLine),
-        );
+        return newLine == null ? ts.sys.newLine : newLine;
     }
 }
