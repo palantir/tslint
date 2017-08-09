10f191beb3696ba12677aea78d43635d4935545b
Aaron Ervin <aervin@uste3.com>
2017-08-09 08:15:10 -0400

    isolated test update on branch

--- test/ruleTestRunner.ts
+++ test/ruleTestRunner.ts
@@ -23,9 +23,34 @@ import { consoleTestResultHandler, runTest } from "../src/test";
 /* tslint:disable:no-console */
 console.log();
 console.log(colors.underline("Testing Lint Rules:"));
-/* tslint:enable:no-console */
 
 const testDirectories = glob.sync("test/rules/**/tslint.json").map(path.dirname);
+const rule: string = process.argv[2];
+
+if (rule !== undefined) {
+    const directories = testDirectories.filter(
+        (directory: string) =>
+            rule.toLowerCase() === directory.split("/")[2].toLowerCase(),
+    );
+    if (directories.length === 0) {
+        console.log(`Sorry, no rules found by the name "${rule}"... Exiting status 0.`);
+        process.exit(0);
+    }
+    const start: number = Date.now();
+    for (const testDirectory of directories) {
+        const results = runTest(testDirectory);
+        const didAllTestsPass = consoleTestResultHandler(results);
+        if (!didAllTestsPass) {
+            process.exit(1);
+        }
+    }
+    const end: number = Date.now();
+
+    console.log(`All tests for rule "${rule}" passed!`);
+    console.log();
+    console.log(`It took ${ end - start }ms to run these tests.`);
+    process.exit(0);
+}
 
 for (const testDirectory of testDirectories) {
     const results = runTest(testDirectory);
