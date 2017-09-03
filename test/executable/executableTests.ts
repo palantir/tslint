d5b133585cd0ea9b48448e28c454be2dd4638eb2
mmkal <15040698+mmkal@users.noreply.github.com>
2017-09-03 13:25:00 -0400

    chore: specify encoding explicitly when writing file

--- test/executable/executableTests.ts
+++ test/executable/executableTests.ts
@@ -166,7 +166,7 @@ describe("Executable", function(this: Mocha.ISuiteCallbackContext) {
     describe("Config with excluded files", () => {
         it("exits with code 1 if linter options doesn't exclude file with lint errors", (done) => {
             const tempFile = createTempFile("included.ts");
-            fs.writeFileSync(tempFile, "console.log(\"missing semi-colon at end of line\")", "utf8");
+            fs.writeFileSync(tempFile, "console.log(\"missing semi-colon at end of line\")", { encoding: "utf8" });
             execCli(["-c", "./test/config/tslint-exclude.json", tempFile], (err) => {
                 assert.isNotNull(err, "process should exit with error");
                 assert.strictEqual(err.code, 1, "error code should be 1");
@@ -176,7 +176,7 @@ describe("Executable", function(this: Mocha.ISuiteCallbackContext) {
 
         it("exits with code 0 if linter options exclude one file with lint errors", (done) => {
             const tempFile = createTempFile("excluded.ts");
-            fs.writeFileSync(tempFile, "console.log(\"missing semi-colon at end of line\")", "utf8");
+            fs.writeFileSync(tempFile, "console.log(\"missing semi-colon at end of line\")", { encoding: "utf8" });
             execCli(["-c", "./test/config/tslint-exclude-one.json", tempFile], (err) => {
                 assert.isNull(err, "process should exit without an error");
                 done();
@@ -185,7 +185,7 @@ describe("Executable", function(this: Mocha.ISuiteCallbackContext) {
 
         it("exits with code 0 if linter options excludes many files with lint errors", (done) => {
             const tempFiles = [1, 2].map((x) => createTempFile(`excluded${x}.ts`));
-            tempFiles.forEach((f) => fs.writeFileSync(f, "console.log(\"missing semi-colon at end of line\")", "utf8"));
+            tempFiles.forEach((f) => fs.writeFileSync(f, "console.log(\"missing semi-colon at end of line\")", { encoding: "utf8" }));
             execCli(["-c", "./test/config/tslint-exclude-many.json", ...tempFiles], (err) => {
                 assert.isNull(err, "process should exit without an error");
                 done();
