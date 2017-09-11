973bbda2557390f28d5174a2f5f572097eb100e6
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-09-11 17:14:15 +0200

    deprecation: check correct constructor overload (#3203)

--- src/rules/deprecationRule.ts
+++ src/rules/deprecationRule.ts
@@ -16,8 +16,16 @@
  */
 
 import {
-    getDeclarationOfBindingElement, isBindingElement, isCallExpression, isIdentifier, isJsDoc,
-    isPropertyAccessExpression, isTaggedTemplateExpression, isVariableDeclaration, isVariableDeclarationList,
+    getDeclarationOfBindingElement,
+    isBindingElement,
+    isCallExpression,
+    isIdentifier,
+    isJsDoc,
+    isNewExpression,
+    isPropertyAccessExpression,
+    isTaggedTemplateExpression,
+    isVariableDeclaration,
+    isVariableDeclarationList,
 } from "tsutils";
 import * as ts from "typescript";
 import * as Lint from "../index";
@@ -113,7 +121,9 @@ function getCallExpresion(node: ts.Expression): ts.CallLikeExpression | undefine
         node = parent;
         parent = node.parent!;
     }
-    return isTaggedTemplateExpression(parent) || isCallExpression(parent) && parent.expression === node ? parent : undefined;
+    return isTaggedTemplateExpression(parent) || (isCallExpression(parent) || isNewExpression(parent)) && parent.expression === node
+        ? parent
+        : undefined;
 }
 
 function getDeprecation(node: ts.Identifier, tc: ts.TypeChecker): string | undefined {
