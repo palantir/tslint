6bbf7a09ecbfcddd70d666a2d101dd2a628b5e5e
Aaron Ervin <aervin@uste3.com>
2017-10-05 11:51:10 -0400

    optionsDescription update

--- src/rules/maxClassesPerFileRule.ts
+++ src/rules/maxClassesPerFileRule.ts
@@ -37,8 +37,8 @@ export class Rule extends Lint.Rules.AbstractRule {
             Ensures that files have a single responsibility so that that classes each exist in their own files`,
         optionsDescription: Lint.Utils.dedent`
             The one required argument is an integer indicating the maximum number of classes that can appear in a
-            file. An optional argument \`"include-class-expressions"\` can be provided to include class expressions
-            in the overall class count.`,
+            file. An optional argument \`"exclude-class-expressions"\` can be provided to exclude class expressions
+            from the overall class count.`,
         options: {
             type: "array",
             items: [
