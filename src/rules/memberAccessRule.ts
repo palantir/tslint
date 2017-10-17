ebce6603b993d7f7f148c660db73ec4bfee7d37c
Cyril Gandon <cgandon@waya-tech.com>
2017-10-17 09:15:01 +0200

    member-access rule: add new-rule-option check-parameter-property

--- src/rules/memberAccessRule.ts
+++ src/rules/memberAccessRule.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { getChildOfKind, getModifier, getNextToken, getTokenAtPosition, isClassLikeDeclaration, isConstructorDeclaration } from "tsutils";
+import * as utils from "tsutils";
 import * as ts from "typescript";
 
 import { showWarningOnce } from "../error";
@@ -24,11 +24,13 @@ import * as Lint from "../index";
 const OPTION_NO_PUBLIC = "no-public";
 const OPTION_CHECK_ACCESSOR = "check-accessor";
 const OPTION_CHECK_CONSTRUCTOR = "check-constructor";
+const OPTION_CHECK_PARAMETER_PROPERTY = "check-parameter-property";
 
 interface Options {
     noPublic: boolean;
     checkAccessor: boolean;
     checkConstructor: boolean;
+    checkParameterProperty: boolean;
 }
 
 export class Rule extends Lint.Rules.AbstractRule {
@@ -42,15 +44,16 @@ export class Rule extends Lint.Rules.AbstractRule {
 
             * \`"no-public"\` forbids public accessibility to be specified, because this is the default.
             * \`"check-accessor"\` enforces explicit visibility on get/set accessors
-            * \`"check-constructor"\`  enforces explicit visibility on constructors`,
+            * \`"check-constructor"\`  enforces explicit visibility on constructors
+            * \`"check-parameter-property"\`  enforces explicit visibility on parameter properties`,
         options: {
             type: "array",
             items: {
                 type: "string",
-                enum: [OPTION_NO_PUBLIC, OPTION_CHECK_ACCESSOR, OPTION_CHECK_CONSTRUCTOR],
+                enum: [OPTION_NO_PUBLIC, OPTION_CHECK_ACCESSOR, OPTION_CHECK_CONSTRUCTOR, OPTION_CHECK_PARAMETER_PROPERTY],
             },
             minLength: 0,
-            maxLength: 3,
+            maxLength: 4,
         },
         optionExamples: [true, [true, OPTION_NO_PUBLIC], [true, OPTION_CHECK_ACCESSOR]],
         type: "typescript",
@@ -71,35 +74,37 @@ export class Rule extends Lint.Rules.AbstractRule {
         const noPublic = options.indexOf(OPTION_NO_PUBLIC) !== -1;
         let checkAccessor = options.indexOf(OPTION_CHECK_ACCESSOR) !== -1;
         let checkConstructor = options.indexOf(OPTION_CHECK_CONSTRUCTOR) !== -1;
+        let checkParameterProperty = options.indexOf(OPTION_CHECK_PARAMETER_PROPERTY) !== -1;
         if (noPublic) {
-            if (checkAccessor || checkConstructor) {
+            if (checkAccessor || checkConstructor || checkParameterProperty) {
                 showWarningOnce(`Warning: ${this.ruleName} - If 'no-public' is present, it should be the only option.`);
                 return [];
             }
-            checkAccessor = checkConstructor = true;
+            checkAccessor = checkConstructor = checkParameterProperty = true;
         }
         return this.applyWithFunction(sourceFile, walk, {
             checkAccessor,
             checkConstructor,
+            checkParameterProperty,
             noPublic,
         });
     }
 }
 
 function walk(ctx: Lint.WalkContext<Options>) {
-    const { noPublic, checkAccessor, checkConstructor } = ctx.options;
+    const { noPublic, checkAccessor, checkConstructor, checkParameterProperty } = ctx.options;
     return ts.forEachChild(ctx.sourceFile, function recur(node: ts.Node): void {
-        if (isClassLikeDeclaration(node)) {
+        if (utils.isClassLikeDeclaration(node)) {
             for (const child of node.members) {
                 if (shouldCheck(child)) {
                     check(child);
                 }
-            }
-        }
-        if (isConstructorDeclaration(node)) {
-            for (const param of node.parameters) {
-                if (ts.isParameterPropertyDeclaration(param)) {
-                    check(param);
+                if (checkParameterProperty && utils.isConstructorDeclaration(child) && child.body !== undefined) {
+                    for (const param of child.parameters) {
+                        if (utils.isParameterProperty(param)) {
+                            check(param);
+                        }
+                    }
                 }
             }
         }
@@ -125,24 +130,24 @@ function walk(ctx: Lint.WalkContext<Options>) {
         if (Lint.hasModifier(node.modifiers, ts.SyntaxKind.ProtectedKeyword, ts.SyntaxKind.PrivateKeyword)) {
             return;
         }
-        const publicKeyword = getModifier(node, ts.SyntaxKind.PublicKeyword);
+        const publicKeyword = utils.getModifier(node, ts.SyntaxKind.PublicKeyword);
         if (noPublic && publicKeyword !== undefined) {
-            const readonlyKeyword = getModifier(node, ts.SyntaxKind.ReadonlyKeyword);
+            const readonlyKeyword = utils.getModifier(node, ts.SyntaxKind.ReadonlyKeyword);
             // public is not optional for parameter property without the readonly modifier
-            const isPublicOptional = !ts.isParameterPropertyDeclaration(node) || readonlyKeyword !== undefined;
+            const isPublicOptional = node.kind !== ts.SyntaxKind.Parameter || readonlyKeyword !== undefined;
             if (isPublicOptional) {
                 const start = publicKeyword.end - "public".length;
                 ctx.addFailure(
                     start,
                     publicKeyword.end,
                     Rule.FAILURE_STRING_NO_PUBLIC,
-                    Lint.Replacement.deleteFromTo(start, getNextToken(publicKeyword, ctx.sourceFile)!.getStart(ctx.sourceFile)),
+                    Lint.Replacement.deleteFromTo(start, utils.getNextToken(publicKeyword, ctx.sourceFile)!.getStart(ctx.sourceFile)),
                 );
             }
         }
         if (!noPublic && publicKeyword === undefined) {
             const nameNode = node.kind === ts.SyntaxKind.Constructor
-                ? getChildOfKind(node, ts.SyntaxKind.ConstructorKeyword, ctx.sourceFile)!
+                ? utils.getChildOfKind(node, ts.SyntaxKind.ConstructorKeyword, ctx.sourceFile)!
                 : node.name !== undefined ? node.name : node;
             const memberName = node.name !== undefined && node.name.kind === ts.SyntaxKind.Identifier ? node.name.text : undefined;
             ctx.addFailureAtNode(
@@ -155,7 +160,7 @@ function walk(ctx: Lint.WalkContext<Options>) {
 }
 
 function getInsertionPosition(member: ts.ClassElement | ts.ParameterDeclaration, sourceFile: ts.SourceFile): number {
-    const node = member.decorators === undefined ? member : getTokenAtPosition(member, member.decorators.end, sourceFile)!;
+    const node = member.decorators === undefined ? member : utils.getTokenAtPosition(member, member.decorators.end, sourceFile)!;
     return node.getStart(sourceFile);
 }
 
@@ -172,7 +177,7 @@ function typeToString(node: ts.ClassElement | ts.ParameterDeclaration): string {
         case ts.SyntaxKind.SetAccessor:
             return "set property accessor";
         case ts.SyntaxKind.Parameter:
-            return "class parameter property";
+            return "parameter property";
         default:
             throw new Error(`unhandled node type ${ts.SyntaxKind[node.kind]}`);
     }
