1f52341a862af0f5d4c6dd76c843d568e8c6fef8
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-10-20 09:27:34 +0200

    add separate test

--- test/formatters/vsoFormatterTests.ts
+++ test/formatters/vsoFormatterTests.ts
@@ -45,7 +45,27 @@ describe("VSO Formatter", () => {
             getFailureString(TEST_FILE, 2, 12, "mid failure", "mid-name") +
             getFailureString(TEST_FILE, 9,  2,  "last failure", "last-name");
 
-        const actualResult = formatter.format(failures, failures);
+        const actualResult = formatter.format(failures);
+        assert.equal(actualResult, expectedResult);
+    });
+
+    it("does not duplicate output for fixed failures", () => {
+        const maxPosition = sourceFile.getFullWidth();
+
+        const failures = [
+            createFailure(sourceFile, 0, 1, "first failure", "first-name", undefined, "error"),
+            createFailure(sourceFile, 32, 36, "mid failure", "mid-name", undefined, "error"),
+            createFailure(sourceFile, maxPosition - 1, maxPosition, "last failure", "last-name", undefined, "error"),
+        ];
+
+        const expectedResult =
+            getFailureString(TEST_FILE, 1,  1, "first failure", "first-name") +
+            getFailureString(TEST_FILE, 2, 12, "mid failure", "mid-name") +
+            getFailureString(TEST_FILE, 9,  2,  "last failure", "last-name");
+
+        const fixed = failures.slice();
+
+        const actualResult = formatter.format(failures, fixed);
         assert.equal(actualResult, expectedResult);
     });
 
