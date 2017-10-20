96f6ccc83afd5b422338116a7c99b5d00b945b81
Cyril Gandon <cgandon@waya-tech.com>
2017-10-20 16:19:47 +0200

    constraint the rule on a set of operator only

--- src/rules/noSelfCompareRule.ts
+++ src/rules/noSelfCompareRule.ts
@@ -23,17 +23,21 @@ import * as Lint from "../index";
 export class Rule extends Lint.Rules.AbstractRule {
     /* tslint:disable:object-literal-sort-keys */
     public static metadata: Lint.IRuleMetadata = {
-        ruleName: "no-constant-binary-expression",
-        description: "Both sides of a binary expression can't be the same.",
+        ruleName: "no-self-compare",
+        description: "Sides of a comparison operator cannot be the same.",
         optionsDescription: "Not configurable.",
         options: null,
         optionExamples: [true],
+        rationale: Lint.Utils.dedent`
+            Comparaison operator (\`==\`, \`===\`, \`!=\`, \`!==\`, \`>\`, \`>=\`, \`<\` and \`<=\`), should not
+            be use on the same value, the result is expected to be always the same.
+            Code like \`if(a === a) { }\` is likely an error.`,
         type: "typescript",
         typescriptOnly: true,
     };
     /* tslint:enable:object-literal-sort-keys */
 
-    public static FAILURE_STRING = "Both sides of a binary expression can't be the same";
+    public static FAILURE_STRING = "Sides of a comparison operator cannot be the same";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         return this.applyWithFunction(sourceFile, walk);
@@ -43,10 +47,26 @@ export class Rule extends Lint.Rules.AbstractRule {
 function walk(ctx: Lint.WalkContext<void>) {
     return ts.forEachChild(ctx.sourceFile, function cb(node: ts.Node): void {
         if (isBinaryExpression(node)) {
-            if (node.right.getText() === node.left.getText()) {
+            if (shouldCheck(node.operatorToken.kind) && node.right.getText() === node.left.getText()) {
                 ctx.addFailureAtNode(node, Rule.FAILURE_STRING);
             }
         }
         return ts.forEachChild(node, cb);
     });
+}
+
+function shouldCheck(kind: ts.SyntaxKind) {
+    switch (kind) {
+        case ts.SyntaxKind.EqualsEqualsToken:
+        case ts.SyntaxKind.EqualsEqualsEqualsToken:
+        case ts.SyntaxKind.ExclamationEqualsEqualsToken:
+        case ts.SyntaxKind.ExclamationEqualsToken:
+        case ts.SyntaxKind.LessThanToken:
+        case ts.SyntaxKind.LessThanEqualsToken:
+        case ts.SyntaxKind.GreaterThanToken:
+        case ts.SyntaxKind.GreaterThanEqualsToken:
+            return true;
+        default:
+            return false;
+    }
 }
