76067a7ca091174f93c928fbbb80b6fb58bedcb4
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-10-28 00:03:06 +0200

    report errors from parsing and validating tsconfig.json
    [bugfix] show errors when `tsconfig.json` is invalid
    Fixes: #3409

--- src/linter.ts
+++ src/linter.ts
@@ -57,14 +57,32 @@ class Linter {
      * Creates a TypeScript program object from a tsconfig.json file path and optional project directory.
      */
     public static createProgram(configFile: string, projectDirectory: string = path.dirname(configFile)): ts.Program {
-        const { config } = ts.readConfigFile(configFile, ts.sys.readFile);
+        const config = ts.readConfigFile(configFile, ts.sys.readFile);
+        if (config.error !== undefined) {
+            throw new FatalError(ts.formatDiagnostic(config.error, {
+                getCanonicalFileName: (f) => f,
+                getCurrentDirectory: process.cwd,
+                getNewLine: () => "\n",
+            }));
+        }
         const parseConfigHost: ts.ParseConfigHost = {
             fileExists: fs.existsSync,
             readDirectory: ts.sys.readDirectory,
             readFile: (file) => fs.readFileSync(file, "utf8"),
             useCaseSensitiveFileNames: true,
         };
-        const parsed = ts.parseJsonConfigFileContent(config, parseConfigHost, path.resolve(projectDirectory), {noEmit: true});
+        const parsed = ts.parseJsonConfigFileContent(config.config, parseConfigHost, path.resolve(projectDirectory), {noEmit: true});
+        if (parsed.errors !== undefined) {
+            // ignore warnings and 'TS18003: No inputs were found in config file ...'
+            const errors = parsed.errors.filter((d) => d.category === ts.DiagnosticCategory.Error && d.code !== 18003);
+            if (errors.length !== 0) {
+                throw new FatalError(ts.formatDiagnostics(errors, {
+                    getCanonicalFileName: (f) => f,
+                    getCurrentDirectory: process.cwd,
+                    getNewLine: () => "\n",
+                }));
+            }
+        }
         const host = ts.createCompilerHost(parsed.options, true);
         const program = ts.createProgram(parsed.fileNames, parsed.options, host);
 
