0b7edfc3974e143a089acd666e63bd8da1563113
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-10-31 20:50:14 +0100

    no-redundant-jsdoc: handle class tag (#3414)

--- src/rules/noRedundantJsdocRule.ts
+++ src/rules/noRedundantJsdocRule.ts
@@ -75,6 +75,7 @@ function walk(ctx: Lint.WalkContext<void>): void {
                 // OK
                 break;
 
+            case ts.SyntaxKind.JSDocClassTag:
             case ts.SyntaxKind.JSDocTemplateTag:
             case ts.SyntaxKind.JSDocTypeTag:
             case ts.SyntaxKind.JSDocTypedefTag:
