ad9030bf1aabafd65903e177f5bb029b7affbcbf
Jason Killian <jkillian@palantir.com>
2017-11-10 22:01:07 +0000

    fixer impl

--- src/rules/fileHeaderRule.ts
+++ src/rules/fileHeaderRule.ts
@@ -18,6 +18,8 @@
 import * as ts from "typescript";
 import * as Lint from "../index";
 
+const ONLY_WHITESPACE = /^\s*$/;
+
 export class Rule extends Lint.Rules.AbstractRule {
     /* tslint:disable:object-literal-sort-keys */
     public static metadata: Lint.IRuleMetadata = {
@@ -25,7 +27,18 @@ export class Rule extends Lint.Rules.AbstractRule {
         description: "Enforces a certain header comment for all files, matched by a regular expression.",
         optionsDescription: "Regular expression to match the header.",
         options: {
-            type: "string",
+            type: "array",
+            items: [
+                {
+                    type: "string",
+                },
+                {
+                    type: "string",
+                },
+            ],
+            additionalItems: false,
+            minLength: 1,
+            maxLength: 2,
         },
         optionExamples: [[true, "Copyright \\d{4}"]],
         type: "style",
@@ -37,19 +50,50 @@ export class Rule extends Lint.Rules.AbstractRule {
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         const { text } = sourceFile;
+        const testRegex = new RegExp(this.ruleArguments[0] as string);
+        const insertText = this.ruleArguments[1] as string | undefined;
+
         // ignore shebang if it exists
         let offset = text.startsWith("#!") ? text.indexOf("\n") : 0;
         // returns the text of the first comment or undefined
-        const commentText = ts.forEachLeadingCommentRange(
+        const commentDetails  = ts.forEachLeadingCommentRange(
             text,
             offset,
-            (pos, end, kind) => text.substring(pos + 2, kind === ts.SyntaxKind.SingleLineCommentTrivia ? end : end - 2));
-        if (commentText === undefined || !new RegExp(this.ruleArguments[0] as string).test(commentText)) {
-            if (offset !== 0) {
-                ++offset; // show warning in next line after shebang
+            (pos, end, kind) => ({pos, end, kind}));
+
+        if (offset !== 0) {
+            ++offset; // show warning in next line after shebang
+        }
+        if (commentDetails === undefined) {
+            const fix = insertText !== undefined
+                ? Lint.Replacement.appendText(offset, this.createComment(sourceFile, insertText))
+                : undefined;
+            return [new Lint.RuleFailure(sourceFile, offset, offset, Rule.FAILURE_STRING, this.ruleName, fix)];
+        } else {
+            const { pos, end, kind } = commentDetails;
+            const commentText = text.substring(pos, kind === ts.SyntaxKind.SingleLineCommentTrivia ? end : end - 2);
+            if (!testRegex.test(commentText)) {
+                const hasContentBeforeComment = !ONLY_WHITESPACE.test(text.substring(offset, pos));
+                const fix = insertText !== undefined
+                    ? hasContentBeforeComment
+                        ? Lint.Replacement.appendText(offset, this.createComment(sourceFile, insertText))
+                        : Lint.Replacement.replaceFromTo(pos, end, this.createComment(sourceFile, insertText, false))
+                    : undefined;
+                return [new Lint.RuleFailure(sourceFile, offset, offset, Rule.FAILURE_STRING, this.ruleName, fix)];
             }
-            return [new Lint.RuleFailure(sourceFile, offset, offset, Rule.FAILURE_STRING, this.ruleName)];
         }
         return [];
     }
+
+    private createComment(sourceFile: ts.SourceFile, commentText: string, trailingNewline=true) {
+        const maybeCarriageReturn = sourceFile.text[sourceFile.getLineEndOfPosition(0)] === "\r" ? "\r" : "";
+        const lineEnding = `${maybeCarriageReturn}\n`;
+        return [
+            '/*',
+            ...commentText.split(lineEnding).map(l => ` * ${l}`),
+            ' */',
+            ...(trailingNewline ? [''] : []),
+        ].join(lineEnding);
+    }
 }
+
