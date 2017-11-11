palantir/tslint

commit 6d3f78e8796879811b823caa2682dfa33e6269e2

Support yaml configuration (#1598) (#3433)

src/configuration.ts
@@ -16,6 +16,7 @@
  */
 
 import * as fs from "fs";
+import * as yaml from "js-yaml";
 import * as path from "path";
 import * as resolve from "resolve";
 import { FatalError, showWarningOnce } from "./error";
@@ -63,7 +64,10 @@ export interface IConfigurationLoadResult {
     results?: IConfigurationFile;
 }
 
-export const CONFIG_FILENAME = "tslint.json";
+// Note: eslint prefers yaml over json, while tslint prefers json over yaml
+// for backward-compatibility.
+export const JSON_CONFIG_FILENAME = "tslint.json";
+export const CONFIG_FILENAMES = [JSON_CONFIG_FILENAME, "tslint.yaml", "tslint.yml"];
 
 export const DEFAULT_CONFIG: IConfigurationFile = {
     defaultSeverity: "error",
@@ -111,7 +115,7 @@ export function findConfiguration(configFile: string | null, inputFilePath?: str
  * the location of the config file is not known and you want to search for one.
  * @param inputFilePath A path to the current file being linted. This is the starting location
  * of the search for a configuration.
- * @returns An absolute path to a tslint.json file
+ * @returns An absolute path to a tslint.json or tslint.yml or tslint.yaml file
  * or undefined if neither can be found.
  */
 export function findConfigurationPath(suppliedConfigFilePath: string | null, inputFilePath: string): string | undefined;
@@ -140,7 +144,7 @@ export function findConfigurationPath(suppliedConfigFilePath: string | null, inp
         }
 
         // search for tslint.json from input file location
-        let configFilePath = findup(CONFIG_FILENAME, path.resolve(inputFilePath!));
+        let configFilePath = findup(CONFIG_FILENAMES, path.resolve(inputFilePath!));
         if (configFilePath !== undefined) {
             return configFilePath;
         }
@@ -148,9 +152,11 @@ export function findConfigurationPath(suppliedConfigFilePath: string | null, inp
         // search for tslint.json in home directory
         const homeDir = getHomeDir();
         if (homeDir != undefined) {
-            configFilePath = path.join(homeDir, CONFIG_FILENAME);
-            if (fs.existsSync(configFilePath)) {
-                return path.resolve(configFilePath);
+            for (const configFilename of CONFIG_FILENAMES) {
+                configFilePath = path.join(homeDir, configFilename);
+                if (fs.existsSync(configFilePath)) {
+                    return path.resolve(configFilePath);
+                }
             }
         }
         // no path could be found
@@ -159,10 +165,11 @@ export function findConfigurationPath(suppliedConfigFilePath: string | null, inp
 }
 
 /**
- * Find a file by name in a directory or any ancestory directory.
+ * Find a file by names in a directory or any ancestor directory.
+ * Will try each filename in filenames before recursing to a parent directory.
  * This is case-insensitive, so it can find 'TsLiNt.JsOn' when searching for 'tslint.json'.
  */
-function findup(filename: string, directory: string): string | undefined {
+function findup(filenames: string[], directory: string): string | undefined {
     while (true) {
         const res = findFile(directory);
         if (res !== undefined) {
@@ -177,18 +184,21 @@ function findup(filename: string, directory: string): string | undefined {
     }
 
     function findFile(cwd: string): string | undefined {
-        if (fs.existsSync(path.join(cwd, filename))) {
-            return filename;
+        const dirFiles = fs.readdirSync(cwd);
+        for (const filename of filenames) {
+            const index = dirFiles.indexOf(filename);
+            if (index > -1) {
+                return filename;
+            }
+            // TODO: remove in v6.0.0
+            // Try reading in the entire directory and looking for a file with different casing.
+            const result = dirFiles.find((entry) => entry.toLowerCase() === filename);
+            if (result !== undefined) {
+                showWarningOnce(`Using mixed case ${filename} is deprecated. Found: ${path.join(cwd, result)}`);
+                return result;
+            }
         }
-
-        // TODO: remove in v6.0.0
-        // Try reading in the entire directory and looking for a file with different casing.
-        const filenameLower = filename.toLowerCase();
-        const result = fs.readdirSync(cwd).find((entry) => entry.toLowerCase() === filenameLower);
-        if (result !== undefined) {
-            showWarningOnce(`Using mixed case tslint.json is deprecated. Found: ${path.join(cwd, result)}`);
-        }
-        return result;
+        return undefined;
     }
 }
 
@@ -207,13 +217,23 @@ export function loadConfigurationFromPath(configFilePath?: string, originalFileP
         return DEFAULT_CONFIG;
     } else {
         const resolvedConfigFilePath = resolveConfigurationPath(configFilePath);
+        const resolvedConfigFileExt = path.extname(resolvedConfigFilePath);
         let rawConfigFile: RawConfigFile;
-        if (path.extname(resolvedConfigFilePath) === ".json") {
-            const fileContent = stripComments(fs.readFileSync(resolvedConfigFilePath)
-                .toString()
-                .replace(/^\uFEFF/, ""));
+        if (/\.(json|ya?ml)/.test(resolvedConfigFileExt)) {
+            const fileContent = fs.readFileSync(resolvedConfigFilePath, "utf8").replace(/^\uFEFF/, "");
             try {
-                rawConfigFile = JSON.parse(fileContent) as RawConfigFile;
+                if (resolvedConfigFileExt === ".json") {
+                    rawConfigFile = JSON.parse(stripComments(fileContent)) as RawConfigFile;
+                } else {
+                    // choose this branch only if /\.ya?ml/.test(resolvedConfigFileExt) === true
+                    rawConfigFile = yaml.safeLoad(fileContent, {
+                        // Note: yaml.LoadOptions expects a schema value of type "any",
+                        // but this trips up the no-unsafe-any rule.
+                        // tslint:disable-next-line:no-unsafe-any
+                        schema: yaml.JSON_SCHEMA,
+                        strict: true,
+                    }) as RawConfigFile;
+                }
             } catch (e) {
                 const error = e as Error;
                 // include the configuration file being parsed in the error since it may differ from the directly referenced config
@@ -286,7 +306,6 @@ export function extendConfigurationFile(targetConfig: IConfigurationFile,
                     }
                 }
             }
-
         }
     }
 

src/runner.ts
@@ -24,10 +24,10 @@ import * as path from "path";
 import * as ts from "typescript";
 
 import {
-    CONFIG_FILENAME,
     DEFAULT_CONFIG,
     findConfiguration,
     IConfigurationFile,
+    JSON_CONFIG_FILENAME,
 } from "./configuration";
 import { FatalError } from "./error";
 import { LintResult } from "./index";
@@ -131,11 +131,11 @@ export async function run(options: Options, logger: Logger): Promise<Status> {
 
 async function runWorker(options: Options, logger: Logger): Promise<Status> {
     if (options.init) {
-        if (fs.existsSync(CONFIG_FILENAME)) {
-            throw new FatalError(`Cannot generate ${CONFIG_FILENAME}: file already exists`);
+        if (fs.existsSync(JSON_CONFIG_FILENAME)) {
+            throw new FatalError(`Cannot generate ${JSON_CONFIG_FILENAME}: file already exists`);
         }
 
-        fs.writeFileSync(CONFIG_FILENAME, JSON.stringify(DEFAULT_CONFIG, undefined, "    "));
+        fs.writeFileSync(JSON_CONFIG_FILENAME, JSON.stringify(DEFAULT_CONFIG, undefined, "    "));
         return Status.Ok;
     }
 

test/config/tslint-invalid.yaml
@@ -1,0 +1,1 @@
+{{ hello I am invalid }

test/config/tslint-with-comments.yaml
@@ -1,0 +1,17 @@
+---
+jsRules:
+  # rule-one: true
+  rule-two:
+    severity: error # after comment
+  rule-three:
+    - true
+    - "#not a comment"
+  # a noice comment
+rules:
+  # rule-one: true
+  rule-two:
+    severity: error # after comment
+  rule-three:
+    - true
+    - "#not a comment"
+...

test/configurationTests.ts
@@ -274,6 +274,16 @@ describe("Configuration", () => {
                 path.resolve("./test/files/config-findup/tslint.json"),
             );
         });
+        it("prefers json over yaml over yml configuration files", () => {
+            assert.strictEqual(
+                findConfigurationPath(null, "./test/files/config-findup/yaml-config"),
+                path.resolve("test/files/config-findup/yaml-config/tslint.json"),
+            );
+            assert.strictEqual(
+                findConfigurationPath(null, "./test/files/config-findup/yml-config"),
+                path.resolve("test/files/config-findup/yml-config/tslint.yaml"),
+            );
+        });
     });
 
     describe("loadConfigurationFromPath", () => {
@@ -419,6 +429,17 @@ describe("Configuration", () => {
 
         it("can load .json files with BOM", () => {
             assert.doesNotThrow(() => loadConfigurationFromPath("./test/config/tslint-with-bom.json"));
+        });
+
+        it("can load .yaml files with comments", () => {
+            const config = loadConfigurationFromPath("./test/config/tslint-with-comments.yaml");
+
+            const expectedConfig = getEmptyConfig();
+            expectedConfig.rules.set("rule-two", { ruleSeverity: "error" });
+            expectedConfig.rules.set("rule-three", { ruleSeverity: "error", ruleArguments: ["#not a comment"] });
+
+            assertConfigEquals(config.rules, expectedConfig.rules);
+            assertConfigEquals(config.jsRules, expectedConfig.rules);
         });
 
         it("can load a built-in configuration", () => {

test/executable/executableTests.ts
@@ -87,12 +87,23 @@ describe("Executable", function(this: Mocha.ISuiteCallbackContext) {
             });
         });
 
-        it("exits with code 1 if config file is invalid", (done) => {
+        it("exits with code 1 if json config file is invalid", (done) => {
             execCli(["-c", "test/config/tslint-invalid.json", "src/test.ts"], (err, stdout, stderr) => {
                 assert.isNotNull(err, "process should exit with error");
                 assert.strictEqual(err.code, 1, "error code should be 1");
 
-                assert.include(stderr, "Failed to load", "stderr should contain notification about failing to load json");
+                assert.include(stderr, "Failed to load", "stderr should contain notification about failing to load json config");
+                assert.strictEqual(stdout, "", "shouldn't contain any output in stdout");
+                done();
+            });
+        });
+
+        it("exits with code 1 if yaml config file is invalid", (done) => {
+            execCli(["-c", "test/config/tslint-invalid.yaml", "src/test.ts"], (err, stdout, stderr) => {
+                assert.isNotNull(err, "process should exit with error");
+                assert.strictEqual(err.code, 1, "error code should be 1");
+
+                assert.include(stderr, "Failed to load", "stderr should contain notification about failing to load yaml config");
                 assert.strictEqual(stdout, "", "shouldn't contain any output in stdout");
                 done();
             });
@@ -103,7 +114,7 @@ describe("Executable", function(this: Mocha.ISuiteCallbackContext) {
                 assert.isNotNull(err, "process should exit with error");
                 assert.strictEqual(err.code, 1, "error code should be 1");
 
-                assert.include(stderr, "Failed to load", "stderr should contain notification about failing to load json");
+                assert.include(stderr, "Failed to load", "stderr should contain notification about failing to load json config");
                 assert.include(stderr, "tslint-invalid.json", "stderr should mention the problem file");
                 assert.strictEqual(stdout, "", "shouldn't contain any output in stdout");
                 done();

test/files/config-findup/yaml-config/tslint.yaml
@@ -1,0 +1,2 @@
+---
+...

test/files/config-findup/yaml-config/tslint.yml
@@ -1,0 +1,2 @@
+---
+...

commit a41ad247721dd07228914dab48b940f170b4900a

revert breaking change introduced in #3433
Restores the removed export `CONFIG_FILENAME` to avoid breaking API users.
Ref: https://github.com/palantir/tslint/pull/3433#discussion_r150042495

[no-log]

src/configuration.ts
@@ -67,6 +67,8 @@ export interface IConfigurationLoadResult {
 // Note: eslint prefers yaml over json, while tslint prefers json over yaml
 // for backward-compatibility.
 export const JSON_CONFIG_FILENAME = "tslint.json";
+/** @deprecated use `JSON_CONFIG_FILENAME` or `CONFIG_FILENAMES` instead. */
+export const CONFIG_FILENAME = JSON_CONFIG_FILENAME;
 export const CONFIG_FILENAMES = [JSON_CONFIG_FILENAME, "tslint.yaml", "tslint.yml"];
 
 export const DEFAULT_CONFIG: IConfigurationFile = {