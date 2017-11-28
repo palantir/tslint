1bd48b5121e0a3de79e2d9391eb1f3686c86ca88
Josh Goldberg <joshuakgoldberg@outlook.com>
2017-11-28 04:22:00 -0800

    Added prefer-readonly rule with fixes (#2896)

--- src/language/typeUtils.ts
+++ src/language/typeUtils.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2016 Palantir Technologies, Inc.
+ * Copyright 2017 Palantir Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,23 +17,23 @@
 
 import * as ts from "typescript";
 
-import { IOptions } from "../rule/rule";
-import { RuleWalker } from "./ruleWalker";
-
-export class ProgramAwareRuleWalker extends RuleWalker {
-    private readonly typeChecker: ts.TypeChecker;
-
-    constructor(sourceFile: ts.SourceFile, options: IOptions, private readonly program: ts.Program) {
-        super(sourceFile, options);
-
-        this.typeChecker = program.getTypeChecker();
+export const typeIsOrHasBaseType = (type: ts.Type, parentType: ts.Type) => {
+    if (type.symbol === undefined || parentType.symbol === undefined) {
+        return false;
     }
 
-    public getProgram(): ts.Program {
-        return this.program;
+    const typeAndBaseTypes = [type];
+    const ancestorTypes = type.getBaseTypes();
+
+    if (ancestorTypes !== undefined) {
+        typeAndBaseTypes.push(...ancestorTypes);
     }
 
-    public getTypeChecker(): ts.TypeChecker {
-        return this.typeChecker;
+    for (const baseType of typeAndBaseTypes) {
+        if (baseType.symbol !== undefined && baseType.symbol.name === parentType.symbol.name) {
+            return true;
+        }
     }
-}
+
+    return false;
+};
