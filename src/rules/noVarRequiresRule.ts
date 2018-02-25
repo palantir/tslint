8772016890424f8d5a8688283470ddf6587569d4
Josh Goldberg <josh@fullscreenmario.com>
2018-02-25 15:52:27 -0800

    Added a couple extra docs to no-var-requires

--- src/rules/noVarRequiresRule.ts
+++ src/rules/noVarRequiresRule.ts
@@ -26,10 +26,17 @@ export class Rule extends Lint.Rules.AbstractRule {
         description: "Disallows the use of require statements except in import statements.",
         descriptionDetails: Lint.Utils.dedent`
             In other words, the use of forms such as \`var module = require("module")\` are banned.
-            Instead use ES6 style imports or \`import foo = require('foo')\` imports.`,
+            Instead use ES2015-style imports or \`import foo = require('foo')\` imports.`,
         optionsDescription: "Not configurable.",
         options: null,
         optionExamples: [true],
+        rationale: Lint.Utils.dedent`
+            AMD-style \`require([])\` and CommonJS-style \`require("")\` statements are environment-specific
+            and more difficult to statically analyze.
+
+            ES2015-style \`import\`s are part of the JavaScript language specfication and recommended as the path going forward.
+            TypeScript will compile them to environment-specific forms as needed.
+        `,
         type: "typescript",
         typescriptOnly: true,
     };
