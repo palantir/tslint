1a0fe45c7539a3e5ef76f58a05896a470297ed93
Michael Black <michael.bees@gmail.com>
2018-07-27 16:30:10 -0400

    Add comment-type rule (#4008)

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -188,6 +188,7 @@ export const rules = {
         "check-space",
         "check-uppercase",
     ],
+    "comment-type": [true, "singleline", "multiline", "doc", "directive"],
     "completed-docs": true,
     // "file-header": No sensible default
     "deprecation": true,
