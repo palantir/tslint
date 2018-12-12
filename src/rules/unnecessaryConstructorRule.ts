4222d32d83eb2cd1efb7545ba1d7536cf41315ca
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-12-12 09:12:14 -0800

    Added missing 'and'

--- src/rules/unnecessaryConstructorRule.ts
+++ src/rules/unnecessaryConstructorRule.ts
@@ -28,7 +28,7 @@ export class Rule extends Lint.Rules.AbstractRule {
         optionsDescription: "Not configurable.",
         rationale: Lint.Utils.dedent`
             JavaScript implicitly adds a blank constructor when there isn't one.
-            It's not necessary to manually one in.
+            It's not necessary to manually add one in.
         `,
         ruleName: "unnecessary-constructor",
         type: "functionality",
