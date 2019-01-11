2aa4947127d36adcd581c32c2ec712b0bfd89ca4
Thomas den Hollander <denhollander.thomas@gmail.com>
2019-01-11 09:14:58 +0100

    Address review

--- test/rules/no-unsafe-any/unknown/commonjsModule.ts
+++
@@ -1,3 +0,0 @@
-const x: any = 0;
-namespace x {}
-export = x;
--- test/rules/no-unsafe-any/unknown/es6Module.ts
+++
@@ -1,8 +0,0 @@
-const defaultExport: any = 0;
-export default defaultExport;
-export const namedExport: any = 0;
-export type T = number;
-
-export namespace NS {
-    export interface ITest {}
-}
