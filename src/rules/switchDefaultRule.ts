03a68df8cc3593cc8307f0753d8bb563b3672370
Michael Hewson <michael@michaelhewson.ca>
2019-01-18 21:22:45 -0500

    finish the allow-exhaustive option for the switch-default rule
    - supports all literal types (`string`, `number` and `bigint`), `true`, `false`, `null`, and `undefined`
    - if the allow-exhaustive option is enabled, but type information is unavailable, the rule is not applied and an error message is displayed

--- src/rules/switchDefaultRule.ts
+++ src/rules/switchDefaultRule.ts
@@ -15,9 +15,12 @@
  * limitations under the License.
  */
 
-import { isDefaultClause, isTypeFlagSet } from "tsutils";
+import { inspect } from "util";
+
+import { isDefaultClause, isTypeFlagSet, unionTypeParts } from "tsutils";
 import * as ts from "typescript";
 
+import { showWarningOnce } from "../error";
 import * as Lint from "../index";
 import { codeExamples } from "./code-examples/switchDefault.examples";
 
@@ -36,7 +39,7 @@ export class Rule extends Lint.Rules.OptionallyTypedRule {
             One argument may be optionally provided:
 
             * \`"${OPTION_ALLOW_EXHAUSTIVE}"\` doesn't require the \`default\` case if all possible expression values\
-are covered by existing \`case\` labels.`,
+are covered by existing \`case\` labels. Requires type information.`,
         options: {
             type: "array",
             items: {
@@ -53,12 +56,42 @@ are covered by existing \`case\` labels.`,
     };
     /* tslint:enable:object-literal-sort-keys */
 
-    public static FAILURE_STRING = "Switch statement should include a 'default' case";
+    public static FAILURE_STRING = "Switch statement should include a `default` case";
+
+    public static failureStringForMissingCases(cases: SingletonValue[]): string {
+        let casesString: string;
+
+        if (cases.length === 0) {
+            throw Error("There should be no failure if there are no missing cases");
+        } else if (cases.length === 1) {
+            casesString = `case \`${inspect(cases[0])}\``;
+        } else {
+            // tslint:disable-next-line: no-unnecessary-callback-wrapper
+            const firstCases: string[] = cases.slice(0, -1).map(x => inspect(x));
+            const lastCase = inspect(cases[cases.length - 1]);
+
+            casesString = `cases \`${firstCases.join(", ")}\` and \`${lastCase}\``;
+        }
+        return `Switch statement is not exhaustive. Add the missing ${casesString} or a \`default\` case.`;
+    }
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithFunction(sourceFile, walk, {
-            allowExhaustive: false, // This option requires TypeChecker
-        });
+        const options = parseOptions(this.ruleArguments);
+
+        if (options.allowExhaustive) {
+            // allow-exhaustive is enabled, but we don't have type information
+            showWarningOnce(
+                Lint.Utils.dedent`
+                    ${this.ruleName} needs type info to use "${OPTION_ALLOW_EXHAUSTIVE}".
+                    See https://palantir.github.io/tslint/usage/type-checking/ for documentation on
+                    how to enable this feature.
+                `,
+            );
+
+            return [];
+        }
+
+        return this.applyWithFunction(sourceFile, walk, options);
     }
 
     public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
@@ -73,49 +106,110 @@ are covered by existing \`case\` labels.`,
     }
 }
 
+function parseOptions(ruleArguments: any[]): Options {
+    return {
+        allowExhaustive: ruleArguments.indexOf(OPTION_ALLOW_EXHAUSTIVE) !== -1,
+    };
+}
+
 function walk(ctx: Lint.WalkContext<Options>, checker?: ts.TypeChecker) {
     return ts.forEachChild(ctx.sourceFile, function cb(node: ts.Node): void {
-        if (
-            ts.isSwitchStatement(node)
-            && !node.caseBlock.clauses.some(isDefaultClause)
-        ) {
-            const failure = !checker || !ctx.options.allowExhaustive
-                ? `${Rule.FAILURE_STRING}`
-                : checkForUncoveredValues(
-                    checker.getTypeAtLocation(node.expression),
-                    node.caseBlock,
-                );
+        if (ts.isSwitchStatement(node) && !node.caseBlock.clauses.some(isDefaultClause)) {
+            if (!ctx.options.allowExhaustive) {
+                ctx.addFailureAtNode(node, Rule.FAILURE_STRING);
+            } else if (checker === undefined) {
+                throw Error("Expected `checker` to be defined");
+            } else {
+                const uncovered = uncoveredValues(checker, node);
 
-            if (failure !== undefined) {
-                ctx.addFailureAtNode(node, failure);
+                if (uncovered === "non-exhaustive") {
+                    ctx.addFailureAtNode(node, Rule.FAILURE_STRING);
+                } else if (uncovered.length > 0) {
+                    ctx.addFailureAtNode(node, Rule.failureStringForMissingCases(uncovered));
+                }
             }
         }
         return ts.forEachChild(node, cb);
     });
 }
 
-function checkForUncoveredValues(exprType: ts.Type, caseBlock: ts.CaseBlock): string | void {
-    if (!isTypeFlagSet(exprType, ts.TypeFlags.Union)) {
-        return Rule.FAILURE_STRING;
-    }
+// returns a (possibly empty) array of missing values, or the string 'non-exhaustive' if the
+// type of the switched expression can't possibly be matched exhaustively
+function uncoveredValues(
+    checker: ts.TypeChecker,
+    switchStatement: ts.SwitchStatement,
+): "non-exhaustive" | SingletonValue[] {
+    const switchedExprType = checker.getTypeAtLocation(switchStatement.expression);
 
-    for (const type of (exprType as ts.UnionType).types) {
-        if (!isTypeFlagSet(type, ts.TypeFlags.StringLiteral)) {
-            return Rule.FAILURE_STRING;
+    const possibleValues: SingletonValue[] = [];
+    for (const type of unionTypeParts(switchedExprType)) {
+        // Note: `boolean` is represented as `true | false` under the hood, so we don't have to
+        // do anything extra to support it
+        const value = singletonValueOfType(type);
+
+        if (value === undefined) {
+            return "non-exhaustive";
         }
 
-        const text = (type as ts.StringLiteralType).value;
-        const hasSuchClause = caseBlock.clauses.some(
-            (clause) => ts.isCaseClause(clause)
-                && ts.isStringLiteral(clause.expression)
-                && (clause.expression.text === text),
-        );
-        if (!hasSuchClause) {
-            return failureStringWithMissingCase(text);
-        }
+        possibleValues.push(value.value);
     }
 
-    function failureStringWithMissingCase(missingCase: string) {
-        return `${Rule.FAILURE_STRING}, at least the '${missingCase}' case is missing`;
+    const caseClauses = switchStatement.caseBlock.clauses as ts.NodeArray<ts.CaseClause>;
+
+    const caseValues: SingletonValue[] = [];
+    for (const clause of caseClauses) {
+        const type = checker.getTypeAtLocation(clause.expression);
+        const value = singletonValueOfType(type);
+
+        if (value === undefined) {
+            return "non-exhaustive";
+        }
+
+        caseValues.push(value.value);
+    }
+
+    // PseudoBigInt is an object, and indexOf will use reference equality. But this should be OK
+    // because AFAICT TypeScript interns all literal types in a Map, and if the type checker returns
+    // the same type twice, it will be the same object (and therefore that object's `value` field
+    // will be the same, too).
+    return possibleValues.filter(value => caseValues.indexOf(value) === -1);
+}
+
+// ts.PseudoBigInt was added in Typescript 3.2, and is a possible `LiteralType` value.
+// Since it is not available in earlier versions, we copy its definition and use it here.
+interface PseudoBigInt {
+    negative: boolean;
+    base10Value: string;
+}
+
+type SingletonValue = string | number | PseudoBigInt | true | false | null | undefined;
+
+// if `type` is a singleton type (a literal, `true`, `false`, `null`, or `undefined`),
+// returns an object whose `value` field is the single value that occupies that type.
+// Otherwise, returns `undefined`.
+// The object wrapper is to distinguish between `undefined` and `{value: undefined}`.
+function singletonValueOfType(type: ts.Type): { value: SingletonValue } | undefined {
+    if (isTypeFlagSet(type, ts.TypeFlags.Null)) {
+        return { value: null };
+    } else if (isTypeFlagSet(type, ts.TypeFlags.Undefined)) {
+        return { value: undefined };
+    } else if (isTypeFlagSet(type, ts.TypeFlags.BooleanLiteral)) {
+        // ts.IntrinsicType is marked as @internal, but AFAIK this is the only way to tell the
+        // difference between `true` and `false`.
+        const name = (type as ts.IntrinsicType).intrinsicName;
+        switch (name) {
+            case "true":
+                return { value: true };
+            case "false":
+                return { value: false };
+            default:
+                throw Error(
+                    `Expected boolean literal type to be "true" or "false", found "${name}"`,
+                );
+        }
+    } else if (type.isLiteral()) {
+        return { value: type.value };
+    } else {
+        return undefined;
     }
 }
