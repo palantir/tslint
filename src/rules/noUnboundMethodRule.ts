1e3c9359f5de78bb93bfcc4bbb8a8952e9b8671c
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-01-20 22:02:17 +0100

    Handle method reference in whitelisted function call

--- src/rules/noUnboundMethodRule.ts
+++ src/rules/noUnboundMethodRule.ts
@@ -198,6 +198,11 @@ function isWhitelisted(node: ts.Node, whitelist: string[]): boolean {
     if (node.parent.kind === ts.SyntaxKind.TypeOfExpression) {
         return whitelist.indexOf("typeof") !== -1;
     }
+    if (node.parent.kind === ts.SyntaxKind.CallExpression) {
+        const parent = node.parent as ts.CallExpression;
+        const expression = parent.expression as ts.Identifier;
+        const callingMethodName = expression.escapedText as string;
+        return whitelist.indexOf(callingMethodName) !== -1;
+    }
     return false;
 }
-
