6b2e2a64a816782e62db1205781591df49502b96
AndreasGassmann <andreas@andreasgassmann.ch>
2019-02-15 02:13:20 +0100

    Revert styling

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -36,8 +36,8 @@ export const rules = {
             ["Boolean", "Avoid using the `Boolean` type. Did you mean `boolean`?"],
             ["Number", "Avoid using the `Number` type. Did you mean `number`?"],
             ["String", "Avoid using the `String` type. Did you mean `string`?"],
-            ["Symbol", "Avoid using the `Symbol` type. Did you mean `symbol`?"]
-        ]
+            ["Symbol", "Avoid using the `Symbol` type. Did you mean `symbol`?"],
+        ],
     },
     "ban-ts-ignore": true,
     "member-access": {
