cbb926a8816629f9f265ad5a516083dfe55458bb
Adi Dahiya <adahiya@palantir.com>
2019-04-16 16:41:11 -0400

    Print out the duplicates

--- scripts/verifyCleanLockfile.sh
+++ scripts/verifyCleanLockfile.sh
@@ -7,9 +7,12 @@ set -o pipefail
 
 scripts_dir="$( dirname "$(readlink -f "$0")" )"
 bin="$scripts_dir/../node_modules/.bin"
+duplicates="$("$bin/yarn-deduplicate" "$scripts_dir/../yarn.lock" --list)"
 
-if [[ $($bin/yarn-deduplicate --list) ]]; then
+if [[ $duplicates ]]; then
     echo "Found duplicate blocks in yarn.lock which can be cleaned up. Please run 'yarn-deduplicate yarn.lock --strategy fewer'"
+    echo ""
+    echo "$duplicates"
     exit 1
 else
     exit 0
