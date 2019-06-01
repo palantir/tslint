6668053e3425870d0724efb9030404f5944714b2
Carlos Ortiz García <goodwine@google.com>
2019-06-01 09:22:45 -0700

    Add fixer for no-void-expression
    Fixer only fixes short-hand arrow functions and return statements.

--- src/rules/noVoidExpressionRule.ts
+++ src/rules/noVoidExpressionRule.ts
@@ -15,10 +15,10 @@
  * limitations under the License.
  */
 
-import { isTypeFlagSet } from "tsutils";
+import {isNodeFlagSet, isTypeFlagSet} from 'tsutils';
 import * as ts from "typescript";
-
 import * as Lint from "../index";
+
 
 const OPTION_IGNORE_ARROW_FUNCTION_SHORTHAND = "ignore-arrow-function-shorthand";
 
@@ -27,6 +27,7 @@ export class Rule extends Lint.Rules.TypedRule {
     public static metadata: Lint.IRuleMetadata = {
         ruleName: "no-void-expression",
         description: "Requires expressions of type `void` to appear in statement position.",
+        hasFix: true,
         optionsDescription: Lint.Utils.dedent`
             If \`${OPTION_IGNORE_ARROW_FUNCTION_SHORTHAND}\` is provided, \`() => returnsVoid()\` will be allowed.
             Otherwise, it must be written as \`() => { returnsVoid(); }\`.`,
@@ -87,9 +88,9 @@ function walk(ctx: Lint.WalkContext<Options>, checker: ts.TypeChecker): void {
         if (
             isPossiblyVoidExpression(node) &&
             !isParentAllowedVoid(node) &&
-            isTypeFlagSet(checker.getTypeAtLocation(node), ts.TypeFlags.Void)
+            isVoidExpression(node, checker)
         ) {
-            ctx.addFailureAtNode(node, Rule.FAILURE_STRING);
+            ctx.addFailureAtNode(node, Rule.FAILURE_STRING, fix(node));
         }
         return ts.forEachChild(node, cb);
     });
@@ -121,6 +122,62 @@ function isPossiblyVoidExpression(node: ts.Node): boolean {
         case ts.SyntaxKind.TaggedTemplateExpression:
             return true;
         default:
-            return false;
+            return ts.isArrowFunction(node.parent) &&
+                isNodeFlagSet(node.parent, ts.NodeFlags.HasImplicitReturn);
     }
+}
+
+function fix(node: ts.Node): Lint.Fix | undefined {
+    let text: string;
+    switch (node.parent.kind) {
+        case ts.SyntaxKind.ReturnStatement:
+            text = `${node.getText()}; return;`;
+            node = node.parent;
+            break;
+        case ts.SyntaxKind.ArrowFunction:
+            text = `{ ${node.getText()}; }`;
+            break;
+        default:
+            return undefined;
+    }
+    return new Lint.Replacement(node.getStart(), node.getWidth(), text);
+}
+
+function isVoidExpression(node: ts.Node, checker: ts.TypeChecker) {
+    if (isTypeFlagSet(checker.getTypeAtLocation(node), ts.TypeFlags.Void)) return true;
+
+    // Detect an arrow function that is up-casted to return void by looking at
+    // the parameter declarations of the function or constructor.
+    // Arrow functions that already have a body wrapped in a block are ignored.
+    if (ts.isArrowFunction(node.parent) &&
+        node.parent.body === node &&
+        !ts.isBlock(node) &&
+        ts.isCallOrNewExpression(node.parent.parent)
+    ) {
+        if (!node.parent.parent.arguments) return false;
+        const idx = node.parent.parent.arguments.findIndex(n => n === node.parent);
+        if (idx === -1) return false;
+        const signature = checker.getResolvedSignature(node.parent.parent)
+        if (!signature) return false;
+        const params = signature.getParameters();
+        if (!params.length) return false;
+        const param = params[idx] || params[params.length - 1];
+        if (!ts.isParameter(param.valueDeclaration) || !param.valueDeclaration.type) return false;
+        let returnType: ts.TypeNode|undefined;
+        // Param is variadic
+        if (param.valueDeclaration.dotDotDotToken &&
+            ts.isTypeReferenceNode(param.valueDeclaration.type) &&
+            param.valueDeclaration.type.typeName.getText() === "Array" &&
+            param.valueDeclaration.type.typeArguments &&
+            param.valueDeclaration.type.typeArguments[0] &&
+            ts.isFunctionTypeNode(param.valueDeclaration.type.typeArguments[0])
+        ) {
+            returnType = (param.valueDeclaration.type.typeArguments[0] as ts.FunctionTypeNode).type
+        } else if (ts.isFunctionTypeNode(param.valueDeclaration.type)) {
+            returnType = param.valueDeclaration.type.type
+        }
+        if(!returnType) return false;
+        return isTypeFlagSet(checker.getTypeFromTypeNode(returnType), ts.TypeFlags.Void);
+    }
+    return false;
 }
