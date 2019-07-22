f7ddea60b1bfe8ea09010dccb566331772bb1594
Max Sysoev <colchgh@gmail.com>
2019-07-23 00:57:03 +0300

    test: add strict-string-expressions into all.ts

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -150,6 +150,7 @@ export const rules = {
     "restrict-plus-operands": true,
     "static-this": true,
     "strict-boolean-expressions": true,
+    "strict-string-expressions": true,
     "strict-comparisons": true,
     "strict-type-predicates": true,
     "switch-default": true,
