6648d3883ac5804aac256cc0fd6b7172f29a62c3
Josh Goldberg <josh@fullscreenmario.com>
2019-10-07 06:06:35 -0400

    Prettier src\formatters\verboseFormatter.ts

--- src/formatters/verboseFormatter.ts
+++ src/formatters/verboseFormatter.ts
@@ -24,8 +24,7 @@ export class Formatter extends AbstractFormatter {
     public static metadata: IFormatterMetadata = {
         formatterName: "verbose",
         description: "The human-readable formatter which includes the rule name in messages.",
-        descriptionDetails:
-            "Basic formatter which outputs simple human-readable messages",
+        descriptionDetails: "Basic formatter which outputs simple human-readable messages",
         sample: "ERROR: (semicolon) myFile.ts[1, 14]: Missing semicolon",
         consumer: "human",
     };
